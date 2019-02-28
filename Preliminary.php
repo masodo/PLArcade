7cf2737a41b3de6a2d0a55cbb1df2df1076df0de
masodo <41118685+masodo@users.noreply.github.com>
2019-02-28 12:19:03 -0500

    Update Preliminary.php

--- Preliminary.php
+++ Preliminary.php
@@ -1,10 +1,10 @@
 <?php
 //-----------------------------------------------------------------------------------/
-//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 © Jcink.com
+//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 Â© Jcink.com
 //Tournaments & JS By: SeanJ. - Heavily Modified by PracticalLightning Web Design
 //Michael S. DeBurger [DeBurger Photo Image & Design]
 //-----------------------------------------------------------------------------------/
-//  phpQuickArcade v3.0.x © Jcink 2005-2010 quickarcade.jcink.com                        
+//  phpQuickArcade v3.0.x Â© Jcink 2005-2010 quickarcade.jcink.com                        
 //
 //  Version: 3.0.23 Final. Released: Sunday, May 02, 2010
 //-----------------------------------------------------------------------------------/
@@ -34,6 +34,8 @@ function bbcodeHtml($str) {
   // delete 'http://' because will be added when convert the code
   $str = str_replace('[url=http://', '[url=', $str);
   $str = str_replace('[url]http://', '[url]', $str);
+  $str = str_replace('[url=https://', '[url=', $str);
+  $str = str_replace('[url]https://', '[url]', $str);
   // Array with RegExp to recognize the code that must be converted
   $bbcode_smiles = array(
     // RegExp for [b]...[/b], [i]...[/i], [u]...[/u], [block]...[/block], [color=code]...[/color], [br]
@@ -101,7 +103,7 @@ if(isset($_GET['play'])) {
 setcookie("gname", $_GET['play']);
 }
 //-----------------------------------------------------------------------------------/
-//  phpQuickArcade v3.0.x © Jcink 2005-2010 quickarcade.jcink.com                        
+//  phpQuickArcade v3.0.x Â© Jcink 2005-2010 quickarcade.jcink.com                        
 //
 //  Version: 3.0.23 Final. Released: Sunday, May 02, 2010
 //-----------------------------------------------------------------------------------/
