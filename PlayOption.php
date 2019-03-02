290631ebf9c2ff7a7905d0cdae89a7786274b6b2
masodo <41118685+masodo@users.noreply.github.com>
2019-03-02 14:19:21 -0500

    De-bugging

--- PlayOption.php
+++ PlayOption.php
@@ -1,17 +1,17 @@
 <?php
 //-----------------------------------------------------------------------------------/
-//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 © Jcink.com
+//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 Â© Jcink.com
 //Tournaments & JS By: SeanJ. - Heavily Modified by PracticalLightning Web Design
 //Michael S. DeBurger [DeBurger Photo Image & Design]
 //-----------------------------------------------------------------------------------/
-//  phpQuickArcade v3.0.x © Jcink 2005-2010 quickarcade.jcink.com                        
+//  phpQuickArcade v3.0.x Â© Jcink 2005-2010 quickarcade.jcink.com                        
 //
 //  Version: 3.0.23 Final. Released: Sunday, May 02, 2010
 //-----------------------------------------------------------------------------------/
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: PlayOption.php  Function: Game Play Block   Modified: 2/28/2019   By: MaSoDo
+# Section: PlayOption.php  Function: Game Play Block   Modified: 3/2/2019   By: MaSoDo
 
 //modified to play HTML5 Games
 $play = htmlspecialchars($_GET['play'], ENT_QUOTES);
@@ -74,7 +74,7 @@ while($f=mysql_fetch_array($q)) echo "<li><b>".$f[0]."</b>: ".$f[1]."</li>";
 <br />
 <?php
 //FSmod addlines
-if (isset($_GET['play'])&&is_numeric(isset($_GET['tournament']))){ ?> <a href="index.php?fullscreen='<?php echo $_GET['play']; ?>'&tournament=<?php echo $_COOKIE['phpqa_tourney']; ?>" target="_blank" title="Fullscreen Mode"><img src="<?php echo $imgloc; ?>/FullScreen.gif" alt="Fullscreen Mode" /></a><br /><?php } else { ?><a href="index.php?fullscreen=<?php echo $_GET['play']; ?>" target="_blank"><img src="<?php echo $imgloc; ?>/FullScreen.gif" alt="Fullscreen Mode" /></a><br /> <?php } 
+if (isset($_GET['play'])&&isset($_GET['tournament'])&&is_numeric($_GET['tournament'])){ ?> <a href="index.php?fullscreen='<?php echo $_GET['play']; ?>'&tournament=<?php echo $_COOKIE['phpqa_tourney']; ?>" target="_blank" title="Fullscreen Mode"><img src="<?php echo $imgloc; ?>/FullScreen.gif" alt="Fullscreen Mode" /></a><br /><?php } else { ?><a href="index.php?fullscreen=<?php echo $_GET['play']; ?>" target="_blank"><img src="<?php echo $imgloc; ?>/FullScreen.gif" alt="Fullscreen Mode" /></a><br /> <?php } 
 //FSmod ?>
 <br />
 <?php 
