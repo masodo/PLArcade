34dbc975eafff6bee96841ace2e9b917a889c59a
masodo <41118685+masodo@users.noreply.github.com>
2019-03-11 13:02:10 -0400

    Update Arcade.php

--- Arcade.php
+++ Arcade.php
@@ -1,17 +1,17 @@
 <?php
 //-----------------------------------------------------------------------------------/
-//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 © Jcink.com
+//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 Â© Jcink.com
 //Tournaments & JS By: SeanJ. - Heavily Modified by PracticalLightning Web Design
 //Michael S. DeBurger [DeBurger Photo Image & Design]
 //-----------------------------------------------------------------------------------/
-//  phpQuickArcade v3.0.x © Jcink 2005-2010 quickarcade.jcink.com                        
+//  phpQuickArcade v3.0.x Â© Jcink 2005-2010 quickarcade.jcink.com                        
 //
 //  Version: 3.0.23 Final. Released: Sunday, May 02, 2010
 //-----------------------------------------------------------------------------------/
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: Arcade.php  Function: Main Organization Page   Modified: 2/28/2019   By: MaSoDo
+# Section: Arcade.php  Function: Main Organization Page   Modified: 3/11/2019   By: MaSoDo
 //-----------------------------------------------------------------------------------/
 require "./Preliminary.php";
 require "./Functions.php";
@@ -32,7 +32,6 @@ elseif (isset($_GET['action']) && $_GET['action'] == "register") require "./Regi
 elseif (isset($_GET['action']) && $_GET['action'] == "Online") require "./OnlineOption.php";
 elseif (isset($_GET['action']) && $_GET['action'] == "profile") require "./ProfileOption.php";
 elseif (isset($_GET['action']) && $_GET['action'] == "leaderboards") require "./LeaderboardsOption.php";
-elseif (isset($_GET['action']) && $_GET['action'] == "tournaments") require "./TournamentOption.php";
 elseif (isset($_GET["action"]) && $_GET['action'] == "settings") require "./SettingsOption.php";
 if (isset($id) || isset($_GET['do']) && $_GET['do'] == "newscore" || isset($_GET['autocom'])) { 
 require "./ScoringOption.php";
