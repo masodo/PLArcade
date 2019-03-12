9ba957e1d884fd665158fe4946f4c8411a70fd96
masodo <41118685+masodo@users.noreply.github.com>
2019-03-12 10:11:10 -0400

    Update addgames.php
    Added New Game Shout Insert

--- acpi/addgames.php
+++ acpi/addgames.php
@@ -1,17 +1,17 @@
 <?php
 //-----------------------------------------------------------------------------------/
-//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 © Jcink.com
+//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 Â© Jcink.com
 //Tournaments & JS By: SeanJ. - Heavily Modified by PracticalLightning Web Design
 //Michael S. DeBurger [DeBurger Photo Image & Design]
 //-----------------------------------------------------------------------------------/
-//  phpQuickArcade v3.0.x © Jcink 2005-2010 quickarcade.jcink.com                        
+//  phpQuickArcade v3.0.x Â© Jcink 2005-2010 quickarcade.jcink.com                        
 //
 //  Version: 3.0.23 Final. Released: Sunday, May 02, 2010
 //-----------------------------------------------------------------------------------/
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: acpi place: addgames Administrator Control Panel   Modified: 2/28/2019   By: MaSoDo
+# Section: acpi place: addgames Administrator Control Panel   Modified: 3/12/2019   By: MaSoDo
 {
 // The different methods
 if (!isset($_GET['method'])) {
@@ -161,6 +161,8 @@ $addedalready = mysql_fetch_array(run_query("SELECT * FROM phpqa_games WHERE gam
 if (empty($addedalready)) {
 message("Game added/edited. <br>[ <a href='index.php?cpiarea=idx'>Arcade CP Home</a> | <a href='index.php?cpiarea=addgames&method=".$_GET['method']."'>Add Another</a> ]");
 run_query("INSERT INTO phpqa_games (game,gameid,gameheight,gamewidth,about,gamecat,remotelink,Champion_name,Champion_score,times_played,platform,scoring) VALUES ('$gamename','$idname','$gameheight','$gamewidth','$about','$gamecat','$remoteurl','$champ','$champs','','$plattype','$scoretype')");
+$NewGtext = "[color=green][i]A new game has been added![/i] [/color][size=18] [url=".$arcurl."/index.php?id=".$idname."][b]".$gamename."[/b][/url][/size] :D [i]Enjoy![/i]";
+run_query("INSERT INTO phpqa_shoutbox (`name`,`shout`,`ipa`) VALUES ('Admin','" . $NewGtext . "','localhost')", 1);
 } else {
 message("This game is already added, or the idname conflicts with an existing game. Please delete the game, or change the idname to correct the problem.");
 }
@@ -290,7 +292,4 @@ echo  "<option value='$catlist[0]'>$catlist[1]</option>";
 <?php
 }
 }
-
-
-
 ?>
