179387af5d07cd689ee6dddfdf7bc34413488517
masodo <41118685+masodo@users.noreply.github.com>
2019-03-14 15:38:52 -0400

    updated for downloads

--- acpi/members.php
+++ acpi/members.php
@@ -1,20 +1,20 @@
 <?php
 //-----------------------------------------------------------------------------------/
-//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 © Jcink.com
+//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 Â© Jcink.com
 //Tournaments & JS By: SeanJ. - Heavily Modified by PracticalLightning Web Design
 //Michael S. DeBurger [DeBurger Photo Image & Design]
 //-----------------------------------------------------------------------------------/
-//  phpQuickArcade v3.0.x © Jcink 2005-2010 quickarcade.jcink.com                        
+//  phpQuickArcade v3.0.x Â© Jcink 2005-2010 quickarcade.jcink.com                        
 //
 //  Version: 3.0.23 Final. Released: Sunday, May 02, 2010
 //-----------------------------------------------------------------------------------/
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: acpi Place: members - Control Panel   Modified: 2/28/2019   By: MaSoDo
+# Section: acpi Place: members - Control Panel   Modified: 3/14/2019   By: MaSoDo
 
 {
-message("View Only: <br /><a href='?cpiarea=members&act=Admin'>Admins</a> &middot; <a href='?cpiarea=members&act=Moderator'>Moderators</a> &middot; <a href='?cpiarea=members&act=Member'>Members</a> &middot; <a href='?cpiarea=members&act=Banned'>Banned</a> &middot; <a href='?cpiarea=members&act=Validating'>Validating</a>");
+message("View Only: <br /><a href='?cpiarea=members&act=Admin'>Admins</a> &middot; <a href='?cpiarea=members&act=Moderator'>Moderators</a> &middot; <a href='?cpiarea=members&act=Affiliate'>Affiliate</a> &middot; <a href='?cpiarea=members&act=Member'>Members</a> &middot; <a href='?cpiarea=members&act=Banned'>Banned</a> &middot; <a href='?cpiarea=members&act=Validating'>Validating</a>");
 if(isset($_POST['members_selected'])) {
 vsess();
 $gselect=$_POST['members_selected'];
@@ -80,10 +80,11 @@ $fg=htmlspecialchars($_POST['chosengroup'], ENT_QUOTES);
 if (isset($_GET['changegroup'])) {
 echo "<div class='tableborder'><table width=100% cellpadding='4' cellspacing='1'><td width=30% align=center class=headertableblock>Change Usergroup of: $_GET[changegroup] </td><tr><td class=arcade1><div align=center><form action='?cpiarea=members&changegroupgo=".htmlspecialchars($_GET['changegroup'])."' method='POST'><input type='hidden' name='akey' value='$key'>";
 ?>
-About: <a href='javascript:alert("As an admin, a user has FULL control of the arcade. Including the ability to add games, make new admins and moderators, and more. Only make admins you trust, proceed with caution.");'>Admin [?]</a> &middot; <a href='javascript:alert("As a moderator, basic access is given. A user has the ability to delete scores, lookup IP addresses, and IP ban users. They are also able to moderate the shoutbox.");'>Moderator [?]</a> &middot; <a href='javascript:alert("A member is the basic group. They can only use the arcade related features such as playing games and using the shoutbox.");'>Member [?]</a> &middot; <a href='javascript:alert("Validating users are members awaiting manual validation. They are blocked from using any means of communication in the arcade, and submitting their highscores.");'>Validating [?]</a> &middot; <a href='javascript:alert("A banned member no longer has access to the arcade. ");'>Banned [?]</a>
+About: <a href='javascript:alert("As an admin, a user has FULL control of the arcade. Including the ability to add games, make new admins and moderators, and more. Only make admins you trust, proceed with caution.");'>Admin [?]</a> &middot; <a href='javascript:alert("As a moderator, basic access is given. A user has the ability to delete scores, lookup IP addresses, and IP ban users. They are also able to moderate the shoutbox.");'>Moderator [?]</a>  &middot; <a href='javascript:alert("An Affiliate has the ability to download the game files. ");'>Affiliate [?]</a>&middot; <a href='javascript:alert("A member is the basic group. They can only use the arcade related features such as playing games and using the shoutbox.");'>Member [?]</a> &middot; <a href='javascript:alert("Validating users are members awaiting manual validation. They are blocked from using any means of communication in the arcade, and submitting their highscores.");'>Validating [?]</a> &middot; <a href='javascript:alert("A banned member no longer has access to the arcade. ");'>Banned [?]</a>
 <select name='chosengroup'>
 <option value='Admin'>Admin</option>
 <option value='Moderator'>Moderator</option>
+<option value='Affiliate'>Afiliate</option>
 <option value='Member'>Member</option>
 <option value='Validating'>Validating</option>
 <option value='Banned'>Banned</option>
