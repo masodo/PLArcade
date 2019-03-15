fa1a34b7cc2012aa0f6d43c854c61744327c8f30
masodo <41118685+masodo@users.noreply.github.com>
2019-03-15 15:50:02 -0400

    debuggung

--- IndexOption.php
+++ IndexOption.php
@@ -1,17 +1,17 @@
 <?php
 //-----------------------------------------------------------------------------------/
-//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 © Jcink.com
+//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 Â© Jcink.com
 //Tournaments & JS By: SeanJ. - Heavily Modified by PracticalLightning Web Design
 //Michael S. DeBurger [DeBurger Photo Image & Design]
 //-----------------------------------------------------------------------------------/
-//  phpQuickArcade v3.0.x © Jcink 2005-2010 quickarcade.jcink.com                        
+//  phpQuickArcade v3.0.x Â© Jcink 2005-2010 quickarcade.jcink.com                        
 //
 //  Version: 3.0.23 Final. Released: Sunday, May 02, 2010
 //-----------------------------------------------------------------------------------/
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: IndexOption.php  Function: Display Games Index   Modified: 2/28/2019   By: MaSoDo
+# Section: IndexOption.php  Function: Display Games Index   Modified: 3/15/2019   By: MaSoDo
 
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	//		  Favorites
@@ -39,7 +39,7 @@ message("Game not found.");
 	//		  Game index display
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 global $catquer;
-if (!isset($_GET['action'])||isset($_GET['action'])&&$_GET['action']!='settings') {
+if (!isset($_GET['action'])&&!isset($_GET['do'])||isset($_GET['action'])&&$_GET['action']!='settings') {
 //if (!empty($catquer)&&isset($_GET['action'])&&$_GET['action']!='settings') {
 if (!empty($catquer)) {
 //echo "<script>alert('Hello World!');</script>";
