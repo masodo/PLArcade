52a075d0d19f9361e9391f9b7e4bcf5636c9456e
masodo <41118685+masodo@users.noreply.github.com>
2019-03-18 10:28:57 -0400

    Hall of Fame Board

---
+++ HOFboardsOption.php
@@ -0,0 +1,44 @@
+<?php
+//-----------------------------------------------------------------------------------/
+//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 © Jcink.com
+//Tournaments & JS By: SeanJ. - Heavily Modified by PracticalLightning Web Design
+//Michael S. DeBurger [DeBurger Photo Image & Design]
+//-----------------------------------------------------------------------------------/
+//  phpQuickArcade v3.0.x © Jcink 2005-2010 quickarcade.jcink.com                        
+//
+//  Version: 3.0.23 Final. Released: Sunday, May 02, 2010
+//-----------------------------------------------------------------------------------/
+// Thanks to (Sean) http://seanj.jcink.com 
+// for: Tournies, JS, and more
+// ---------------------------------------------------------------------------------/
+# Section: Leaderboards.php  Function: Display of Hall of Fame   Modified: 3/18/2019   By: MaSoDo
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//		  Leaderboards
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+$scoreboard = run_query("SELECT phpqa_accounts.name,phpqa_accounts.avatar, COUNT(phpqa_games.HOF_name) AS HOF_champ FROM phpqa_accounts
+LEFT JOIN phpqa_games ON phpqa_accounts.name = phpqa_games.HOF_name
+GROUP BY phpqa_games.HOF_name
+ORDER BY HOF_champ DESC LIMIT 1,50000");
+$scoreboardc = mysql_fetch_array(run_query("SELECT phpqa_accounts.name,phpqa_accounts.avatar, COUNT(phpqa_games.HOF_name) AS HOF_champ FROM phpqa_accounts
+LEFT JOIN phpqa_games ON phpqa_accounts.name = phpqa_games.HOF_name
+GROUP BY phpqa_games.HOF_name
+ORDER BY HOF_champ DESC LIMIT 0,3"));
+if ($scoreboardc['avatar'] == ''){ $scoreboardc['avatar'] = $avatarloc.'/man.gif'; }
+ echo "<br /><div><table width='33%' cellpadding='5' cellspacing='1'>";
+ echo "<tr><td width='100%' align=center class='headertableblock' style='font-size:24px'><img alt='image' src='".$crowndir."/crown1.gif' /> Arcade Hall of Fame Champion: <img image src='".$crowndir."/crown1.gif' /></td></tr>";
+echo "<tr><td class='arcade1'><div style='font-size:16px; color: gold;' align='center'>with <I>".$scoreboardc['HOF_champ']."</I> wins</div><div align='center' style='font-size:24px'><i><A href=\"index.php?action=profile&amp;user=".$scoreboardc['name']."\"><img src='".$scoreboardc['avatar']."' /><br />".$scoreboardc['name']."</a></i></div></td></tr>";
+echo "</table></div><div style='position:absolute;display:none;margin-top:-100px;margin-left:110px;' id='leadboxpopup'><img /></div><br />";
+ echo "<div class='tableborder'><table width='100%' cellpadding='5' cellspacing='1'>";
+ echo "<td width='2%' align=left class='headertableblock'>UsersName</td><td width='30%' align=center class='headertableblock'>Totals</td>";
+while($scores=mysql_fetch_array($scoreboard)){ 
+if ($scores['avatar'] == ''){ $scores['avatar'] = $avatarloc.'/man.gif'; }
+if ($scores['HOF_champ'] > 0) {
+// avatar popup here "leadboxpopup"
+echo"<tr><td class='arcade1'><div align='center'><A href='index.php?action=profile&amp;user=".$scores['name']."' onmouseover=\"s=document.getElementById('leadboxpopup');s.style.display='';s.getElementsByTagName('img')[0].src='" . $scores['avatar'] . "';\" onmousemove=\"s=document.getElementById('leadboxpopup').style;s.top=document.body.scrollTop+2+event.clientY;s.left=document.body.scrollLeft+event.clientX;\" onmouseout=\"document.getElementById('leadboxpopup').style.display='none'\"><b>".$scores['name']."</b></a></div></td><td class='arcade1'><div align='center'>".$scores['HOF_champ']." wins</div></td></td></tr>";
+}}
+echo "</table></div><br />";
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//		End Leaderboards
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+?>
