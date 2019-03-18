ffbfc8207e127b8714865d2a5d922de0390e978c
masodo <41118685+masodo@users.noreply.github.com>
2019-03-18 10:31:26 -0400

    debug

--- ModOption.php
+++ ModOption.php
@@ -1,19 +1,19 @@
 <?php
 //-----------------------------------------------------------------------------------/
-//Practical-Lightning-Arcade [PLA] 1.0 (ALPHA) based on PHP-Quick-Arcade 3.0 © Jcink.com
+//Practical-Lightning-Arcade [PLA] 1.0 (ALPHA) based on PHP-Quick-Arcade 3.0 Â© Jcink.com
 //Tournaments & JS By: SeanJ. - Heavily Modified by PracticalLightning Web Design
 //Michael S. DeBurger [DeBurger Photo Image & Design]
 //-----------------------------------------------------------------------------------/
-//  phpQuickArcade v3.0.x © Jcink 2005-2010 quickarcade.jcink.com                        
+//  phpQuickArcade v3.0.x Â© Jcink 2005-2010 quickarcade.jcink.com                        
 //
 //  Version: 3.0.23 Final. Released: Sunday, May 02, 2010
 //-----------------------------------------------------------------------------------/
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: ModOption.php  Function: Call To Moderator Control Panel   Modified: 2/28/2019   By: MaSoDo
+# Section: ModOption.php  Function: Call To Moderator Control Panel   Modified: 3/18/2019   By: MaSoDo
 
-if ($exist[6] == "Moderator" || $exist[6] == "Admin") { 
+if ($exist['group'] == "Moderator" || $exist['group'] == "Admin") { 
 require("acpmoderate.php");
 } else {
 message("Only moderators may access the ModCP");
