c0d3e1680362ed09d1613a27d66719f5752ca951
masodo <41118685+masodo@users.noreply.github.com>
2019-03-18 16:11:19 -0400

    style fix

--- SmileyPop.php
+++ SmileyPop.php
@@ -14,7 +14,7 @@
 # Section: SmileyPop.php  Function: Emoticon Picker Popup   Modified: 3/18/2019   By: MaSoDo
 
 if(isset($_GET['action']) && $_GET['action']=="emotes") {
-echo "<div class='tableborder' width='75%' style='margin-top: 10px; margin-right: auto; margin-left: auto;'><table width='100%' cellpadding='4' cellspacing='1'><tr><td width='60%' align='center' class='headertableblock'>Emote</td><td width='60%' align='center' class='headertableblock'>Symbol</td></tr><tr>";
+echo "<div class='tableborder' width='75%' style='margin-top: 10px; margin-right: auto; margin-left: auto; margin-bottom: 10px;'><table width='100%' cellpadding='4' cellspacing='1'><tr><td width='60%' align='center' class='headertableblock'>Emote</td><td width='60%' align='center' class='headertableblock'>Symbol</td></tr><tr>";
 for($x=1;$x<count($smilies);$x++) {
 $trim = htmlspecialchars(trim($smilies[$x]),ENT_QUOTES);
 echo "\n<tr onclick=\"window.opener.document.forms['boxform'].elements['senttext'].value+='$trim'\"><td class='arcade1' align='left'><img src=\"emoticons/$smiliesp[$x]\"><br /></td><td class='arcade1' align='center'>$trim</td></tr>";
