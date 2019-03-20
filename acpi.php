894b4197e4846257693c489954721476f376fe36
masodo <41118685+masodo@users.noreply.github.com>
2019-03-20 10:42:06 -0400

    Wall of Fame mods

--- acpi.php
+++ acpi.php
@@ -1,18 +1,17 @@
 <?php
 //-----------------------------------------------------------------------------------/
-//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 © Jcink.com
+//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 Â© Jcink.com
 //Tournaments & JS By: SeanJ. - Heavily Modified by PracticalLightning Web Design
 //Michael S. DeBurger [DeBurger Photo Image & Design]
 //-----------------------------------------------------------------------------------/
-//  phpQuickArcade v3.0.x © Jcink 2005-2010 quickarcade.jcink.com                        
+//  phpQuickArcade v3.0.x Â© Jcink 2005-2010 quickarcade.jcink.com                        
 //
 //  Version: 3.0.23 Final. Released: Sunday, May 02, 2010
 //-----------------------------------------------------------------------------------/
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: acp.php  Function: (Experimental) Administrator Control Panel   Modified: 2/28/2019   By: MaSoDo
-
+# Section: acp.php  Function: (Experimental) Administrator Control Panel   Modified: 3/19/2019   By: MaSoDo
 $place = htmlspecialchars($_GET['cpiarea']); 
 if (isset($_REQUEST['acpcheck'])) die();
 if (isset($_COOKIE['acpcheck'])) die();
@@ -69,4 +68,8 @@ require "acpi/Email.php";
 
 if ($place == 'filter') {
 require "acpi/filter.php";
-}?>
+}
+if ($place == 'snapshot') {
+require "acpi/SnapShot.php";
+}
+?>
