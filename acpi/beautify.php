32a908b9c13713083e02ba0a97570c972c2b6a1d
masodo <41118685+masodo@users.noreply.github.com>
2019-05-24 12:46:13 -0400

    Added JavaScript Beautifier Integration

---
+++ acpi/beautify.php
@@ -0,0 +1,30 @@
+<?php
+
+//-----------------------------------------------------------------------------------/
+
+//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 © Jcink.com
+
+//Tournaments & JS By: SeanJ. - Heavily Modified by PracticalLightning Web Design
+
+//Michael S. DeBurger [DeBurger Photo Image & Design]
+
+//-----------------------------------------------------------------------------------/
+
+//  phpQuickArcade v3.0.x © Jcink 2005-2010 quickarcade.jcink.com                        
+
+//
+
+//  Version: 3.0.23 Final. Released: Sunday, May 02, 2010
+
+//-----------------------------------------------------------------------------------/
+
+// Thanks to (Sean) http://seanj.jcink.com 
+
+// for: Tournies, JS, and more
+
+// ---------------------------------------------------------------------------------/
+
+# Section: acpi Place: JSbeautify - Administrator Control Panel   Modified: 5/24/2019   By: MaSoDo
+?>
+
+<iframe src="acpi/js-beautify/index.html" width="1200" height="1200"></iframe>
