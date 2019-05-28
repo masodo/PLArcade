7a3f4a5b325586faec94a3fe8d62fc537135dd75
masodo <41118685+masodo@users.noreply.github.com>
2019-05-28 12:25:54 -0400

    fixed broken SQL Query Submit function

--- acpi/mysql.php
+++ acpi/mysql.php
@@ -1,21 +1,21 @@
 <?php
 //-----------------------------------------------------------------------------------/
-//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 © Jcink.com
+//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 Â© Jcink.com
 //Tournaments & JS By: SeanJ. - Heavily Modified by PracticalLightning Web Design
 //Michael S. DeBurger [DeBurger Photo Image & Design]
 //-----------------------------------------------------------------------------------/
-//  phpQuickArcade v3.0.x © Jcink 2005-2010 quickarcade.jcink.com                        
+//  phpQuickArcade v3.0.x Â© Jcink 2005-2010 quickarcade.jcink.com                        
 //
 //  Version: 3.0.23 Final. Released: Sunday, May 02, 2010
 //-----------------------------------------------------------------------------------/
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: acpi Place: mysql - Administrator Control Panel   Modified: 2/28/2019   By: MaSoDo
+# Section: acpi Place: mysql - Administrator Control Panel   Modified: 5/28/2019   By: MaSoDo
 
 {
-$thequery=isset($_POST['thequery']);
-if(isset($_POST['querymysql']) == "Run Query") {
+if(isset($_POST['querymysql']) && $_POST['querymysql'] == "Run Query") {
+$thequery=$_POST['thequery'];
 	vsess();
 $goquery=run_query($thequery);
 }
