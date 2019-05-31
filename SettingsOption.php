4c6182bf2c046e7d4666a652fb6462396dca64f6
masodo <41118685+masodo@users.noreply.github.com>
2019-05-31 09:48:09 -0400

    fixed reversed default index view selector

--- SettingsOption.php
+++ SettingsOption.php
@@ -11,7 +11,7 @@
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: SettingsOption.php  Function: User Definable Configuration   Modified: 5/30/2019   By: MaSoDo
+# Section: SettingsOption.php  Function: User Definable Configuration   Modified: 5/31/2019   By: MaSoDo
 echo "<!-- SettingsOption.php --------------------------------------------------------------------------------------------------------------------------------------------- -->";
 if (isset($_COOKIE['phpqa_user_c'])) {
 if($exist[6] == "Validating") {
@@ -298,12 +298,12 @@ echo "</select><br /><hr />";
 //
 echo "Selected <b>Games Index</b> View?";
 echo '<select size="1" name="idxview">';
-if ($acct_setting[10] == "GV") {
-echo "<option value='GV' selected>Grid View</option>";
-echo "<option value='CV'>Classic View</option>";
-} else { 
+if ($acct_setting[10] == "CV") {
 echo "<option value='GV'>Grid View</option>";
 echo "<option value='CV' selected>Classic View</option>";
+} else { 
+echo "<option value='GV' selected>Grid View</option>";
+echo "<option value='CV'>Classic View</option>";
 }
 echo "</select><br /><hr />";
 ?>
