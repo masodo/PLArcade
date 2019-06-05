d389aa5e3b675ac54bcc6f547c05dd2eaf32b12b
masodo <41118685+masodo@users.noreply.github.com>
2019-06-05 16:09:43 -0400

    category <div>'s larger
    (and forces icons to 35px X 35px)

--- Categories.php
+++ Categories.php
@@ -1,18 +1,17 @@
 <?php
 //-----------------------------------------------------------------------------------/
-//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 © Jcink.com
+//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 Â© Jcink.com
 //Tournaments & JS By: SeanJ. - Heavily Modified by PracticalLightning Web Design
 //Michael S. DeBurger [DeBurger Photo Image & Design]
 //-----------------------------------------------------------------------------------/
-//  phpQuickArcade v3.0.x © Jcink 2005-2010 quickarcade.jcink.com                        
+//  phpQuickArcade v3.0.x Â© Jcink 2005-2010 quickarcade.jcink.com                        
 //
 //  Version: 3.0.23 Final. Released: Sunday, May 02, 2010
 //-----------------------------------------------------------------------------------/
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: Categories.php  Function: Display Categories w/Icons   Modified: 2/28/2019   By: MaSoDo
-
+# Section: Categories.php  Function: Display Categories w/Icons   Modified: 6/5/2019   By: MaSoDo
 global $catquery;
 $c=@mysql_data_seek($catquery,0);
 if($c) {
@@ -22,7 +21,7 @@ if($c) {
  echo "<table width='100%' cellpadding='4' cellspacing='1' class='tableborder'><tr><td class='headertableblock' style='font-weight:bold; text-align:center;'>Categories:</td><tr><td><div style='width:100%;height:auto;'>";
 while ($c = mysql_fetch_array($catquery)) {
 if ($c[0] != '2') {
-echo  "<div style='width: 45px; height: 45px; padding: 10px; float: left; margin-right: 5px; margin-bottom: 5px; text-align:center;' class='arcade1'><a href='index.php?cat=$c[0]' title='$c[1]'><img src='$catloc/$c[1].png' /><br />$c[1]</a></div>";
+echo  "<div style='width: 55px; height: 55px; padding: 10px; float: left; margin-right: 5px; margin-bottom: 5px; text-align:center;' class='arcade1'><a href='index.php?cat=$c[0]' title='$c[1]'><img src='$catloc/$c[1].png' style='width:35px; height:35px;' /><br />$c[1]</a></div>";
 } }
 echo"</div></td></tr></div></table><br />";
 // End Collapse #3
