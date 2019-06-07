3fde64b9621ec23fe8bf381459c3278813324ff8
masodo <41118685+masodo@users.noreply.github.com>
2019-06-07 09:05:42 -0400

    Bug Fix / Redesign

--- ForgotPassOption.php
+++ ForgotPassOption.php
@@ -1,21 +1,21 @@
 <?php
 //-----------------------------------------------------------------------------------/
-//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 © Jcink.com
+//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 Â© Jcink.com
 //Tournaments & JS By: SeanJ. - Heavily Modified by PracticalLightning Web Design
 //Michael S. DeBurger [DeBurger Photo Image & Design]
 //-----------------------------------------------------------------------------------/
-//  phpQuickArcade v3.0.x © Jcink 2005-2010 quickarcade.jcink.com                        
+//  phpQuickArcade v3.0.x Â© Jcink 2005-2010 quickarcade.jcink.com                        
 //
 //  Version: 3.0.23 Final. Released: Sunday, May 02, 2010
 //-----------------------------------------------------------------------------------/
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: ForgotPassOption.php  Function: Password Recovery   Modified: 2/28/2019   By: MaSoDo
+# Section: ForgotPassOption.php  Function: Password Recovery   Modified: 6/7/2019   By: MaSoDo
 
-if($settings[enable_passrecovery]) {
-if ($_POST['sendto_user']) {
-if ($_POST['sendto_email'] == "") { 
+if($settings['enable_passrecovery']) {
+if (isset($_POST['sendto_user'])) {
+if (isset($_POST['sendto_email'])&&$_POST['sendto_email'] == "") { 
 message("You forgot to type in an email.");
 die();
 }
@@ -34,11 +34,11 @@ message("The email on file for $u, does not match the email you inputted. Please
 die();
 } else {
 // Send the email now...
-$SiteDomain = "http://".htmlspecialchars($_SERVER[HTTP_HOST]).htmlspecialchars($_SERVER[PHP_SELF])."?action=login&userID=$getmailuser[0]&pword=$getmailuser[1]&recovery=1";
-$hd="admin@{$_SERVER[HTTP_HOST]}";
+$SiteDomain = "http://".htmlspecialchars($_SERVER['HTTP_HOST']).htmlspecialchars($_SERVER['PHP_SELF'])."?action=login&userID=$getmailuser[0]&pword=$getmailuser[1]&recovery=1";
+$hd="admin@{$_SERVER['HTTP_HOST']}";
 $members = $getmailuser['email'];
-$mailsub = "Password Recovery From $settings[arcade_title]";
-$mailbody = "Dear $u, \n\r\n\r\n\r A password recovery attempt has been made on your account at the $settings[arcade_title]. If you did not request password recovery ( also known as forgot password form ). If you did not request password recovery, the IP address of the internet user who did this was $_SERVER[REMOTE_ADDR]. Please contact an admin at $settings[arcade_title] about this. \n\r\n\r\n\r Visit the link below to login to your account again: \n\r\r\n ----------------------------------------------- \n\r\n\r $SiteDomain \n\r\n\r-----------------------------------------------\n\r\n\r When you login, visit change password in your profile to setup a new password.";
+$mailsub = "Password Recovery From ".$settings['arcade_title']."";
+$mailbody = "Dear ".$u.", \n\r\n\r\n\r A password recovery attempt has been made on your account at the ".$settings['arcade_title'].". If you did not request password recovery please disregard this email. The IP address of the internet user who did this was ".$_SERVER['REMOTE_ADDR'].". \n\r\n\r\n\r Visit the link below to access your account again: \n\r\r\n ----------------------------------------------- \n\r\n\r ".$SiteDomain." \n\r\n\r-----------------------------------------------\n\r\n\r IMPORTANT: When you login, visit change password in your profile settings to create a new password. \n\r\n\r\n\rThank you for your participation!\n\r".$settings['arcade_title']."";
 $headers = "From: $hd\n\r";
 $mailfail=@mail($members,$mailsub,$mailbody,$headers);
 if($mailfail) { message("Password recovery email sent. You should recieve it in the next 5 minutes to an hour depending on the speed of the mail server."); } else { message("Failed to send the email."); }
