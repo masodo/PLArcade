d5c3f4cb2d78534cc1fb2e352228af72f6278748
masodo <41118685+masodo@users.noreply.github.com>
2019-06-11 11:58:31 -0400

    Admin display dead cats

--- Categories.php
+++ Categories.php
@@ -11,7 +11,7 @@
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: Categories.php  Function: Display Categories w/Icons   Modified: 6/6/2019   By: MaSoDo
+# Section: Categories.php  Function: Display Categories w/Icons   Modified: 6/11/2019   By: MaSoDo
 global $catquery;
 $c=@mysql_data_seek($catquery,0);
 if($c) {
@@ -20,9 +20,13 @@ if($c) {
  echo "<a title='Open/Close The Category Selections'><img id='btn3' src='$imgloc/$collimg3' type='button' alt='&#8595; Categories: Collapse/Expand &#8595;' onclick='return CollapseExpand3()' style='font-size:16px; font-weight:bold; color:silver;' /></a></div><div id='MyDiv3' class='" . $collapset3 . "'>";
  echo "<table width='100%' cellpadding='4' cellspacing='1' class='tableborder'><tr><td class='headertableblock' style='font-weight:bold; text-align:center;'>Categories:</td><tr><td><div style='width:100%;height:auto;'>";
 while ($c = mysql_fetch_array($catquery)) {
+
 if ($c[0] != '2') {
-echo  "<div style='width: 80px; height: 80px; padding: 10px; float: left; margin-right: 5px; margin-bottom: 5px; text-align:center;' class='arcade1'><a href='index.php?cat=$c[0]' title='$c[1]'><img src='$catloc/$c[1].png' style='width:60px; height:60px;' /><br />$c[1]</a></div>";
+echo  "<div style='width: 80px; height: 80px; padding: 10px; float: left; margin-right: 5px; margin-bottom: 5px; text-align:center;' class='arcade1'><a href='index.php?cat=".$c[0]."' title='".$c[1]."'><img src='".$catloc."/".$c[1].".png' style='width:60px; height:60px;' /><br />$c[1]</a></div>";
 } }
+if (isset($exist[6])&&$exist[6] == "Admin") {
+echo  "<div style='width: 80px; height: 80px; padding: 10px; float: left; margin-right: 5px; margin-bottom: 5px; text-align:center;' class='arcade1'><a href='index.php?cat=2' title='dead'><img src='".$catloc."/Dead.png' style='width:60px; height:60px;' /><br />Dead</a></div>";
+}
 echo"</div></td></tr></div></table><br />";
 // End Collapse #3
 echo "</div>";
