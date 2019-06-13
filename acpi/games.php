1034b22ec36987346135360730584281005b1633
masodo <41118685+masodo@users.noreply.github.com>
2019-06-13 18:10:50 -0400

    fixed games wont delete bug

--- acpi/games.php
+++ acpi/games.php
@@ -1,17 +1,17 @@
 <?php
 //-----------------------------------------------------------------------------------/
-//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 © Jcink.com
+//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 Â© Jcink.com
 //Tournaments & JS By: SeanJ. - Heavily Modified by PracticalLightning Web Design
 //Michael S. DeBurger [DeBurger Photo Image & Design]
 //-----------------------------------------------------------------------------------/
-//  phpQuickArcade v3.0.x © Jcink 2005-2010 quickarcade.jcink.com                        
+//  phpQuickArcade v3.0.x Â© Jcink 2005-2010 quickarcade.jcink.com                        
 //
 //  Version: 3.0.23 Final. Released: Sunday, May 02, 2010
 //-----------------------------------------------------------------------------------/
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: acpi Place: games - Administrator Control Panel   Modified: 2/28/2019   By: MaSoDo
+# Section: acpi Place: games - Administrator Control Panel   Modified: 6/13/2019   By: MaSoDo
 
 
 {
@@ -38,8 +38,23 @@ if (isset($_POST['dowhat']))$dowhat = htmlspecialchars($_POST['dowhat'], ENT_QUO
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 if (!is_numeric($dowhat)) {
 if ($dowhat == 'deletegame') {
+function rrmdir($dirgone) { 
+   if (is_dir($dirgone)) { 
+     $objects = scandir($dirgone); 
+     foreach ($objects as $object) { 
+       if ($object != "." && $object != "..") { 
+         if (is_dir($dirgone."/".$object))
+           rrmdir($dirgone."/".$object);
+         else
+           unlink($dirgone."/".$object); 
+       } 
+     }
+     rmdir($dirgone); 
+   } 
+ }
 vsess();
 for($x=0;$x<=count($gselect)-1;$x++){
+rrmdir("./arcade/gamedata/$gselect[$x]");
 @unlink("./arcade/$gselect[$x].swf");
 @unlink("./arcade/pics/$gselect[$x].gif");
 $f=htmlspecialchars($gselect[$x], ENT_QUOTES);
@@ -131,5 +146,4 @@ echo  "<option value='$catlist[0]'>$catlist[1]</option>";
 </td><td width='1px' class='arcade1'><input type='checkbox' onclick="c=document.getElementsByTagName('input');for(x=0;x<c.length;x++) if (c[x].type.toLowerCase()=='checkbox') c[x].checked=this.checked"></td></tr></table></div><br /></form>
 <?php
 	}
-
 ?>
