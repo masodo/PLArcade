851932cd85fe3fb4d367b6990e58da4bc1bbee2b
masodo <41118685+masodo@users.noreply.github.com>
2019-06-19 09:04:21 -0400

    repair job :/

--- acpi/SnapShot.php
+++ acpi/SnapShot.php
@@ -1,17 +1,17 @@
 <?php
 //-----------------------------------------------------------------------------------/
-//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 © Jcink.com
+//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 Â© Jcink.com
 //Tournaments & JS By: SeanJ. - Heavily Modified by PracticalLightning Web Design
 //Michael S. DeBurger [DeBurger Photo Image & Design]
 //-----------------------------------------------------------------------------------/
-//  phpQuickArcade v3.0.x © Jcink 2005-2010 quickarcade.jcink.com                        
+//  phpQuickArcade v3.0.x Â© Jcink 2005-2010 quickarcade.jcink.com                        
 //
 //  Version: 3.0.23 Final. Released: Sunday, May 02, 2010
 //-----------------------------------------------------------------------------------/
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: Leaderboards.php  Function: Display of Hall of Fame   Modified: 3/18/2019   By: MaSoDo
+# Section: Leaderboards.php  Function: Display of Hall of Fame   Modified: 6/19/2019   By: MaSoDo
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //		  SnapShot
@@ -24,10 +24,7 @@ $Wyear = date("Y");
 if ($WYsee['Wyear'] == $Wyear) { die("<script>alert('We Already Have This SnapShot!')</script>"); }
 }
 
-$Wscoreboard = run_query("SELECT phpqa_accounts.name,phpqa_accounts.avatar, COUNT(phpqa_leaderboard.username) AS champions FROM phpqa_accounts
-LEFT JOIN phpqa_leaderboard ON phpqa_accounts.name = phpqa_leaderboard.username
-GROUP BY phpqa_leaderboard.username
-ORDER BY champions DESC LIMIT 0,3");
+$Wscoreboard = run_query("SELECT `name`,`avatar`, COUNT(`phpqa_leaderboard`.`username`) AS `champions` FROM `phpqa_accounts` LEFT JOIN `phpqa_leaderboard` ON `phpqa_accounts`.`name` = `phpqa_leaderboard`.`username` GROUP BY `phpqa_leaderboard`.`username` ORDER BY `champions` DESC LIMIT 0,3");
 $Wscores=mysql_fetch_array($Wscoreboard);
 
 
@@ -40,7 +37,7 @@ $Wgames = $Wscores['champions'];
 $Wname = $Wscores['name'];
 $Wavatar = $Wscores['avatar'];
 
-run_query("INSERT INTO `phpqa_wall` (`Wyear`,`Wplace`,`Wgames`,`Wname`,`Wavatar`) VALUES ($Wyear,$Wplace,$Wgames,'$Wname','$Wavatar')");
+run_query("INSERT INTO `phpqa_wall` (`Wyear`,`Wplace`,`Wgames`,`Wname`,`Wavatar`) VALUES (".$Wyear.",".$Wplace.",".$Wgames.",'".$Wname."','".$Wavatar."')");
 
 while($Wscores=mysql_fetch_array($Wscoreboard)){
 $trop = $trop + 1;
@@ -49,8 +46,7 @@ $Wplace = $trop;
 $Wgames = $Wscores['champions'];
 $Wname = $Wscores['name'];
 $Wavatar = $Wscores['avatar'];
-
-run_query("INSERT INTO `phpqa_wall` (`Wyear`,`Wplace`,`Wgames`,`Wname`,`Wavatar`) VALUES ($Wyear,$Wplace,$Wgames,'$Wname','$Wavatar')");
+run_query("INSERT INTO `phpqa_wall` (`Wyear`,`Wplace`,`Wgames`,`Wname`,`Wavatar`) VALUES (".$Wyear.",".$Wplace.",".$Wgames.",'".$Wname."','".$Wavatar."')");
 }
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //		End SnapShot
