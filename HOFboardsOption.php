54bea155ebdc6514d896ad1631932d375009565f
masodo <41118685+masodo@users.noreply.github.com>
2019-06-19 09:44:15 -0400

    added group style

--- HOFboardsOption.php
+++ HOFboardsOption.php
@@ -11,23 +11,23 @@
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: Leaderboards.php  Function: Display of Hall of Fame   Modified: 3/20/2019   By: MaSoDo
+# Section: Leaderboards.php  Function: Display of Hall of Fame   Modified: 6/19/2019   By: MaSoDo
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //		  Leaderboards
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-$scoreboard = run_query("SELECT phpqa_accounts.name,phpqa_accounts.avatar, COUNT(phpqa_games.HOF_name) AS HOF_champ FROM phpqa_accounts
+$scoreboard = run_query("SELECT phpqa_accounts.name,phpqa_accounts.avatar,phpqa_accounts.group, COUNT(phpqa_games.HOF_name) AS HOF_champ FROM phpqa_accounts
 LEFT JOIN phpqa_games ON phpqa_accounts.name = phpqa_games.HOF_name
 GROUP BY phpqa_games.HOF_name
 ORDER BY HOF_champ DESC LIMIT 1,50000");
-$scoreboardc = mysql_fetch_array(run_query("SELECT phpqa_accounts.name,phpqa_accounts.avatar, COUNT(phpqa_games.HOF_name) AS HOF_champ FROM phpqa_accounts
+$scoreboardc = mysql_fetch_array(run_query("SELECT phpqa_accounts.name,phpqa_accounts.avatar,phpqa_accounts.group, COUNT(phpqa_games.HOF_name) AS HOF_champ FROM phpqa_accounts
 LEFT JOIN phpqa_games ON phpqa_accounts.name = phpqa_games.HOF_name
 GROUP BY phpqa_games.HOF_name
 ORDER BY HOF_champ DESC LIMIT 0,3"));
 if ($scoreboardc['avatar'] == ''){ $scoreboardc['avatar'] = $avatarloc.'/man.gif'; }
  echo "<br /><div><table width='33%' cellpadding='5' cellspacing='1'>";
  echo "<tr><td width='100%' align=center class='headertableblock' style='font-size:24px'><img alt='image' src='".$crowndir."/crown1.gif' /> Hall of Fame Champion: <img image src='".$crowndir."/crown1.gif' /></td></tr>";
-echo "<tr><td class='arcade1'><div style='font-size:16px; color: gold;' align='center'>With <I>".$scoreboardc['HOF_champ']."</I> Games</div><div align='center' style='font-size:24px'><i><a href=\"index.php?action=profile&amp;user=".$scoreboardc['name']."\"><img src='".$scoreboardc['avatar']."' /><br />".$scoreboardc['name']."</a></i></div></td></tr>";
+echo "<tr><td class='arcade1'><div style='font-size:16px; color: gold;' align='center'>With <I>".$scoreboardc['HOF_champ']."</I> Games</div><div align='center' style='font-size:24px'><i><a href=\"index.php?action=profile&amp;user=".$scoreboardc['name']."\"><img src='".$scoreboardc['avatar']."' /><br /><span class='".$scoreboardc['group']."Look'>".$scoreboardc['name']."</span></a></i></div></td></tr>";
 echo "</table></div><div style='position:absolute;display:none;margin-top:-100px;margin-left:110px;' id='leadboxpopup'><img /></div><br />";
  echo "<div class='tableborder'><table width='100%' cellpadding='5' cellspacing='1'>";
  echo "<td width='2%' align=left class='headertableblock'>UsersName</td><td width='30%' align=center class='headertableblock'>Totals</td>";
@@ -35,7 +35,7 @@ while($scores=mysql_fetch_array($scoreboard)){
 if ($scores['avatar'] == ''){ $scores['avatar'] = $avatarloc.'/man.gif'; }
 if ($scores['HOF_champ'] > 0) {
 // avatar popup here "leadboxpopup"
-echo"<tr><td class='arcade1'><div align='center'><A href='index.php?action=profile&amp;user=".$scores['name']."' onmouseover=\"s=document.getElementById('leadboxpopup');s.style.display='';s.getElementsByTagName('img')[0].src='" . $scores['avatar'] . "';\" onmousemove=\"s=document.getElementById('leadboxpopup').style;s.top=document.body.scrollTop+2+event.clientY;s.left=document.body.scrollLeft+event.clientX;\" onmouseout=\"document.getElementById('leadboxpopup').style.display='none'\"><b>".$scores['name']."</b></a></div></td><td class='arcade1'><div align='center'>".$scores['HOF_champ']." Games</div></td></td></tr>";
+echo"<tr><td class='arcade1'><div align='center'><A href='index.php?action=profile&amp;user=".$scores['name']."' onmouseover=\"s=document.getElementById('leadboxpopup');s.style.display='';s.getElementsByTagName('img')[0].src='" . $scores['avatar'] . "';\" onmousemove=\"s=document.getElementById('leadboxpopup').style;s.top=document.body.scrollTop+2+event.clientY;s.left=document.body.scrollLeft+event.clientX;\" onmouseout=\"document.getElementById('leadboxpopup').style.display='none'\"><b><span class='".$scores['group']."Look'>".$scores['name']."</span></b></a></div></td><td class='arcade1'><div align='center'>".$scores['HOF_champ']." Games</div></td></td></tr>";
 }}
 echo "</table></div><br />";
 
