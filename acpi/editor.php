83ffa1bf7e8d3e6d31fd60fbe03f853973703b03
masodo <41118685+masodo@users.noreply.github.com>
2019-06-20 12:14:10 -0400

    announcement feature

--- acpi/editor.php
+++ acpi/editor.php
@@ -1,18 +1,19 @@
 <?php
 //-----------------------------------------------------------------------------------/
-//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 © Jcink.com
+//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 Â© Jcink.com
 //Tournaments & JS By: SeanJ. - Heavily Modified by PracticalLightning Web Design
 //Michael S. DeBurger [DeBurger Photo Image & Design]
 //-----------------------------------------------------------------------------------/
-//  phpQuickArcade v3.0.x © Jcink 2005-2010 quickarcade.jcink.com                        
+//  phpQuickArcade v3.0.x Â© Jcink 2005-2010 quickarcade.jcink.com                        
 //
 //  Version: 3.0.23 Final. Released: Sunday, May 02, 2010
 //-----------------------------------------------------------------------------------/
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: acpi Place: editor - Administrator Control Panel   Modified: 2/28/2019   By: MaSoDo
+# Section: acpi Place: editor - Administrator Control Panel   Modified: 6/20/2019   By: MaSoDo
 
+if(!isset($_GET['announce'])){
 { 
 $skin=htmlspecialchars($_GET['skin'], ENT_QUOTES);
 $skin=str_replace("..","", $skin);
@@ -28,7 +29,19 @@ if($acss) { message("Changes written successfully"); } else { message("Changes f
 $ARCADECSS = @file_get_contents("./skins/$skin");
 if($ARCADECSS=="") $ARCADECSS="File not found";
  message("<form method='post' action='?cpiarea=editor&skin=$skin'><input type='hidden' name='akey' value='$key'><textarea rows='40' cols='60' name='cssforarcade'>$ARCADECSS</textarea><br /><input type='submit' name='skinedit' value='Edit Stylesheet'></form>");
-} 
+} } else {
+{ 
+$Afile=htmlspecialchars($_GET['announce'], ENT_QUOTES);
+$Afile=str_replace("..","", $Afile);
 
-
+if (isset($_POST['fileedit'])) {
+vsess();
+$annf = @fopen("./".$textloc."/".$Afile."","w");
+fwrite($annf,stripslashes($_POST['annedits']));
+if($annf) { message("Changes written successfully"); } else { message("Changes failed to be written. Please check permissions on your skin files and folder."); }
+}
+$ARCADEANN = @file_get_contents("./".$textloc."/".$Afile."");
+if($ARCADEANN=="") $ARCADEANN="File not found";
+ message("<form method='post' action='?cpiarea=editor&announce=".$Afile."'><input type='hidden' name='akey' value='$key'><textarea rows='40' cols='60' name='annedits'>$ARCADEANN</textarea><br /><input type='submit' name='fileedit' value='Edit Announcement'></form>");
+} }
 ?>
