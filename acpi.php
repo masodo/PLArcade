07128d12769ee26bb177471e793a33de8ab8d266
masodo <41118685+masodo@users.noreply.github.com>
2019-06-20 12:17:52 -0400

    announcement feature

--- acpi.php
+++ acpi.php
@@ -11,7 +11,7 @@
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: acp.php  Function: (Experimental) Administrator Control Panel   Modified: 5/24/2019   By: MaSoDo
+# Section: acp.php  Function: (Experimental) Administrator Control Panel   Modified: 6/20/2019   By: MaSoDo
 $place = htmlspecialchars($_GET['cpiarea']); 
 if (isset($_REQUEST['acpcheck'])) die();
 if (isset($_COOKIE['acpcheck'])) die();
@@ -26,7 +26,7 @@ require "acpi/tar_import.php";
 require "acpi/tar_importH5.php";
 } elseif($place == "skin") {
 require "acpi/skin.php";
-}
+} 
 
 if ($place == "editor") {
 require "acpi/editor.php";
