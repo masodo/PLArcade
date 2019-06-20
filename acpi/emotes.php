942de3ff658252b68a1743f53335770f70211648
masodo <41118685+masodo@users.noreply.github.com>
2019-06-20 08:30:06 -0400

    database driven smileys

--- acpi/emotes.php
+++ acpi/emotes.php
@@ -11,68 +11,47 @@
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: acpi Place: emotes - Administrator Control Panel   Modified: 2/28/2019   By: MaSoDo
+# Section: acpi Place: emotes - Administrator Control Panel   Modified: 6/19/2019   By: MaSoDo
 
 { ?>
 <?php
-$remove=isset($_GET['remove']);
 if (isset($_GET['remove'])) {
-$file = $textloc."/emotes_faces.txt";
-$array=file($file); 
-$file2=fopen($file, "w"); 
-foreach($array as $k=>$v){ 
-if ($k == $remove || $v == "" ){ 
-continue; 
-} 
-fwrite($file2, $v); 
-} 
-fclose($file2); 
-$file = $textloc."/emotes_pics.txt";
-$array=file($file); 
-$file2=fopen($file, "w"); 
-foreach($array as $k=>$v){ 
-if ($k == $remove || $v == "" ){ 
-continue; 
-} 
-fwrite($file2, $v); 
-} 
-fclose($file2);
+vsess();
+$remove = $_GET['remove'];
+run_query("delete FROM `phpqa_emotes` where `filename`='".$remove."'");
 }
 if (isset($_POST['face']))$face = $_POST['face'];
 if (isset($_POST['pic']))$pic = $_POST['pic'];
+if (isset($_POST['description']))$description = $_POST['description'];
 if (isset($_POST['face'])) {
 vsess();
-$smilefile = fopen($textloc."/emotes_faces.txt", "a");
-fwrite($smilefile,  "$face\n" );
-$imagefile = fopen($textloc."/emotes_pics.txt", "a");
-fwrite($imagefile,  "$pic\n" );
+run_query("INSERT into `phpqa_emotes` (`id`,`filename`,`code`,`description`) VALUES ('','".$pic."','".$face."','".$description."')");
 }
-$smilies = file($textloc."/emotes_faces.txt");
-$smiliesp = file($textloc."/emotes_pics.txt");
+$emotesdata = run_query("SELECT * FROM `phpqa_emotes`");
 ?>
 <br>
 <?php
-echo "<div class='tableborder'><table width=100% cellpadding='4' cellspacing='1'><td width=60% align=center class=headertableblock>Face</td><td width=10% align=center class=headertableblock>Preview</td><td width=60% align=center class=headertableblock>Remove</td>";
+echo "<div class='tableborder'><table width=50% cellpadding='4' cellspacing='1'><td width=20% align=center class=headertableblock>Face</td><td width=500% align=center class=headertableblock>Description</td><td width=10% align=center class=headertableblock>Preview</td><td width=60% align=center class=headertableblock>Remove</td>";
 	// display
-for($x=1;$x<count($smilies);$x++) {
-echo "<tr><td class=arcade1>$smilies[$x]</td><td class=arcade1><div align=center><img src='emoticons/$smiliesp[$x]'></div></td><td class=arcade1><a href='?cpiarea=emotes&remove=$x'>Remove</a></td></tr>";
+while($smils=mysql_fetch_array($emotesdata)){
+echo "<tr><td class=arcade1>".$smils['code']."</td><td class=arcade1><div align=center>".$smils['description']."</div></td><td class=arcade1><div align=center><img src='".$smiliesloc."/".$smils['filename']."'></div></td><td class=arcade1><a href='?cpiarea=emotes&remove=".$smils['filename']."'>Remove</a></td></tr>";
 }
-echo "</table></div>";
+echo "</table><p>&nbsp;</p></div>";
 ?>
 <form action='index.php?cpiarea=emotes' method='POST'>
 <input type='hidden' name='akey' value='<?php echo $key; ?>'>
 <br />
 <div class='tableborder'><table width=100% cellpadding='4' cellspacing='1'><td width=60% align=center class=headertableblock>Add Emoticon</td><tr><td class=arcade1>
-<div align=center>Face/action:<input type=text name=face>
+<div align=center>Code:<input type='text' name='face'> Description:<input type='text' name='description'>
 Image: 
 	    <select name="pic">
 <?php
 echo "<option value=':)' selected>Select an image</option>";
-$dir = "./emoticons/";
+$dir = "./".$smiliesloc."/";
    if ($dh = opendir($dir)) {
        while (($file = readdir($dh)) !== false) {
  if ($file == "." || $file == "..") continue;
-echo "<option value='$file'>$file</option>";
+echo "<option value='".$file."'>".$file."</option>";
        }
        closedir($dh);
    }
@@ -81,7 +60,16 @@ echo "<option value='$file'>$file</option>";
 <input type='Submit' name='Submit' value='Add Emoticon'></div>
 </td></tr></table></div>
 </form>
-<?php } 
-
-
+<?php
+}
+echo "<h2>Hover over image below to see it's filename. Find it to select above.</h2>";
+$dir2 = "./".$smiliesloc."/";
+   if ($dh2 = opendir($dir)) {
+       while (($file = readdir($dh2)) !== false) {
+ if ($file == "." || $file == "..") continue;
+ echo "<a title='".$file."'><img src='".$smiliesloc."/".$file."' /></a></option>";
+ }
+       closedir($dh2);
+ 
+}
 ?>
