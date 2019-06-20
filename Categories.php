9bf8f64da425aed44e22b2666af4fb7a45fb1604
masodo <41118685+masodo@users.noreply.github.com>
2019-06-20 10:14:09 -0400

    game counts added

--- Categories.php
+++ Categories.php
@@ -11,11 +11,12 @@
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: Categories.php  Function: Display Categories w/Icons   Modified: 6/19/2019   By: MaSoDo
+# Section: Categories.php  Function: Display Categories w/Icons   Modified: 6/20/2019   By: MaSoDo
 global $catquery;
 $c=@mysql_data_seek($catquery,0);
 $catcell = '';
 $caticon = '';
+$catcnt = '';
 (isset($settings['catdiv'])&&$settings['catdiv']!='')?$catcell = $settings['catdiv']:$catcell = '80';
 (isset($settings['catimg'])&&$settings['catimg']!='')?$caticon = $settings['catimg']:$caticon = '60';
 if($c) {
@@ -24,12 +25,13 @@ if($c) {
  echo "<a title='Open/Close The Category Selections'><img id='btn3' src='$imgloc/$collimg3' type='button' alt='&#8595; Categories: Collapse/Expand &#8595;' onclick='return CollapseExpand3()' style='font-size:16px; font-weight:bold; color:silver;' /></a></div><div id='MyDiv3' class='" . $collapset3 . "'>";
  echo "<table width='100%' cellpadding='4' cellspacing='1' class='tableborder'><tr><td class='headertableblock' style='font-weight:bold; text-align:center;'>Categories:</td><tr><td><div style='width:100%;height:auto;'>";
 while ($c = mysql_fetch_array($catquery)) {
-
+$catcnt = run_query("(SELECT COUNT(*) as total FROM `phpqa_games` WHERE `gamecat` = ".$c[0].")");
+$dcat=mysql_fetch_assoc($catcnt);
 if ($c[0] != '2') {
-echo  "<div style='width: ".$catcell."px; height: ".$catcell."px; padding: 10px; float: left; margin-right: 5px; margin-bottom: 5px; text-align:center;' class='arcade1'><a href='index.php?cat=".$c[0]."' title='".$c[1]."'><img src='".$catloc."/".$c[1].".png' style='width:".$caticon."px; height:".$caticon."px;' /><br />$c[1]</a></div>";
+echo  "<div style='width: ".$catcell."px; height: ".$catcell."px; padding: 10px; float: left; margin-right: 5px; margin-bottom: 5px; text-align:center;' class='arcade1'><a href='index.php?cat=".$c[0]."' title='".$c[1]." (".$dcat['total'].")'><img src='".$catloc."/".$c[1].".png' style='width:".$caticon."px; height:".$caticon."px;' /><br />$c[1]</a><br /><span style='font-size:9px; color:#000;'>(".$dcat['total'].")</span></div>";
 } }
 if (isset($exist[6])&&$exist[6] == "Admin") {
-echo  "<div style='width: ".$catcell."px; height: ".$catcell."px; padding: 10px; float: left; margin-right: 5px; margin-bottom: 5px; text-align:center;' class='arcade1'><a href='index.php?cat=2' title='dead'><img src='".$catloc."/Dead.png' style='width:".$caticon."px; height:".$caticon."px;' /><br />Dead</a></div>";
+echo  "<div style='width: ".$catcell."px; height: ".$catcell."px; padding: 10px; float: left; margin-right: 5px; margin-bottom: 5px; text-align:center;' class='arcade1'><a href='index.php?cat=2' title='dead (".$dcat['total'].")'><img src='".$catloc."/Dead.png' style='width:".$caticon."px; height:".$caticon."px;' /><br />Dead</a><br /><span style='font-size:9px; color:#000;'>(".$dcat['total'].")</span></div>";
 }
 echo"</div></td></tr></div></table><br />";
 // End Collapse #3
