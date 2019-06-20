f6409963877ef4023bbdde9c2a6d50dd3fac22f1
masodo <41118685+masodo@users.noreply.github.com>
2019-06-20 12:20:01 -0400

    announcement feature
    starter announcement file

---
+++ flat/announce.php
@@ -0,0 +1,5 @@
+<div style='margin-top:30px; width:1000px; height:auto; background-color:white; color:black; text-align: justify; margin-left:auto; margin-right:auto; opacity: 0.8; padding:10px; font-size:14px;'>
+<h3>Important Announcement!</h3>
+	<p></p><p><strong class="bbc">Cookies</strong><br>
+<span style="color: #ff0000"><strong class="bbc">This website does not store any information that would, on its own, allow us to identify individual users of this service without their permission. Any cookies that may be used by this website are used either solely on a per session basis or to maintain user preferences. Cookies are not shared with any third parties.</strong>
+</div>
