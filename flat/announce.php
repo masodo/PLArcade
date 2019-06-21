79e1e9c531e6891c7977ae28e3cc864e4835ead2
masodo <41118685+masodo@users.noreply.github.com>
2019-06-21 13:57:38 -0400

    Personal "Welcome Back" message

--- flat/announce.php
+++ flat/announce.php
@@ -1,4 +1,8 @@
 <div style='margin-top:30px; margin-bottom:30px; width:1000px; height:auto; background-color:white; color:black; text-align: justify; margin-left:auto; margin-right:auto; opacity: 0.8; padding:10px; font-size:14px;'>
+<?php 
+if (isset($_COOKIE['phpqa_user_c'])) {
+echo "<h2>Welcome Back ".$phpqa_user_cookie."!</h2>";
+}?>
 <h3>Important Announcement!</h3>
 	<p></p><p><strong class="bbc">Cookies</strong><br>
 <span style="color: #ff0000"><strong class="bbc">This website does not store any information that would, on its own, allow us to identify individual users of this service without their permission. Any cookies that may be used by this website are used either solely on a per session basis or to maintain user preferences. Cookies are not shared with any third parties.</strong>
