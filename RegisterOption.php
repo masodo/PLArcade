064f9618711fc91af458b227f4d4c181307972fa
masodo <41118685+masodo@users.noreply.github.com>
2019-06-27 15:03:42 -0400

    Adding PunBB Forum

--- RegisterOption.php
+++ RegisterOption.php
@@ -11,7 +11,7 @@
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: RegisterOption.php  Function: Register for the Arcade   Modified: 6/17/2019   By: MaSoDo
+# Section: RegisterOption.php  Function: Register for the Arcade   Modified: 6/27/2019   By: MaSoDo
 if (isset($_POST['usernamesign']) && $_POST['usernamesign'] != "" && isset($_POST['postpassword']) && $_POST['postpassword'] !="") {
 $name = htmlspecialchars($_POST['usernamesign'], ENT_QUOTES);
 $pass = md5(sha1(htmlspecialchars($_POST['postpassword'])));
@@ -68,6 +68,7 @@ $headers = "From: ".$hd."\nBcc: ".$siteemail."\n";
 @mail($email,$mailsub,$mailbody,$headers);
 }
 run_query("INSERT INTO `phpqa_accounts` (`name`,`pass`,`email`,`ipaddress`,`avatar`,`group`,`skin`,`settings`) VALUES ('$name','$pass','$email','$ipa','','$status','$defCSS','$s_settings')", 1);
+run_query("INSERT INTO `PLA_users` (`username`,`password`,`email`,`registration_ip`,`group_id`) VALUES ('$name','$pass','$email','$ipa','3')", 1);
 if(isset($settings['enable_email_validation'])&&$settings['enable_email_validation'] != '1') { 
 	message("Welcome to the arcade, <b>$name</b>!<br /><br /> Click the '<i>Login</i>' link above, enter your name and password and login to begin playing!");
 } else {
