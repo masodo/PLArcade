00a610e9205f8e9ecf437ea166e2172014f14014
masodo <41118685+masodo@users.noreply.github.com>
2019-06-27 14:56:24 -0400

    Adding PunBB Forum

---
+++ acpi/exportuserstoforum.php
@@ -0,0 +1,20 @@
+<?php
+$tquery = run_query("select * from `phpqa_accounts`");
+while($truser = mysql_fetch_array($tquery)){
+$usegroup = '';
+$takename = $truser['name'];
+$takepass = $truser['pass'];
+$takeemail = $truser['email'];
+$takegroup = $truser['group'];
+$takeIP = $truser['ipaddress'];
+
+if ($takegroup == 'Member')$usegroup = 3;
+if ($takegroup == 'Admin')$usegroup = 1;
+if ($takegroup == 'Moderator')$usegroup = 4;
+if ($takegroup == 'Affiliate')$usegroup = 5;
+
+run_query("INSERT INTO `PLA_users` VALUES('', ".$usegroup.", '".$takename."', '".$takepass."', NULL, '".$takeemail."', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 'English', 'Oxygen', 1, 0, NULL, NULL, 0, '".$takeIP."', 0, NULL, NULL, NULL, 0, 0, 0);");
+
+}
+echo "<script>alert('DONE!')</script>";
+?>
