1fc0582039a3a5cd3cf8e8e63d568b4b8cfffa76
masodo <41118685+masodo@users.noreply.github.com>
2019-06-27 14:40:42 -0400

    Adding PunBB Forum

---
+++ FORUM/cache/cache_stats.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!defined('FORUM_STATS_LOADED')) define('FORUM_STATS_LOADED', 1);
+
+$forum_stats = array (
+  'total_users' => '10',
+  'last_user' => 
+  array (
+    'id' => '2',
+    'username' => 'admin1',
+  ),
+  'total_topics' => '2',
+  'total_posts' => '2',
+  'cached' => 1561659685,
+);
+
+?>
