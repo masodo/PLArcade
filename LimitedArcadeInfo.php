ab13288f4a01469441709efd6038a500ecebe5b6
masodo <41118685+masodo@users.noreply.github.com>
2019-07-01 14:13:22 -0400

    Gateway to Forum

--- LimitedArcadeInfo.php
+++ LimitedArcadeInfo.php
@@ -11,7 +11,7 @@
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: ArcadeInfo.php  Function: Latest Site Info Block   Modified: 6/27/2019   By: MaSoDo
+# Section: LimitedArcadeInfo.php  Function: For Closed Arcade   Modified: 7/1/2019   By: MaSoDo
 ?>
 <br />
 <div align="center">
@@ -21,7 +21,7 @@
 if (!isset($_COOKIE['phpqa_user_c'])) {
 // begin nav buttons
 ?>
-<div style="width: 100%; text-align: center; margin-left: auto; margin-right: auto;"><a name="Login"></a><div class="navigation">Logged off: [ <a href="javascript:tog('login_form')">Login</a> ]</div><div class="navigation">[ <a href="index.php?action=register#registration">Register</a> ] </div><div class="navigation"><a href="/FORUM">Forum (Separate Login)</a></div>
+<div style="width: 100%; text-align: center; margin-left: auto; margin-right: auto;"><a name="Login"></a><div class="navigation">[ <a href="javascript:tog('login_form')">Login</a> ]</div><div class="navigation">[ <a href="index.php?action=register#registration">Register</a> ] </div>
 <div style="display:none" id="login_form" name="tog_collect"><br /><br /><br /><form method="post" action="?action=login"> Name: <input type="text" name="userID" style="font-size: 80%;" /> <font style="font-size: 80%;"></font> Pass: <input type="password" name="pword" style="font-size: 80%;" /> <input type="submit" value="Login" style="font-size: 80%;" /><br /><input type="checkbox" name="cookiescheck" />Remember Login? | <a href="index.php?action=register#registration"><b>Register to play!</b></a><br /><a href="index.php?action=forgotpass"><i>Forgot Password?</i></a></form></div></div>
 <?php
 }
