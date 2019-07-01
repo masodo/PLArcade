25972856dc2a6f943f8c0a0b9081f49a0d9c1ac2
masodo <41118685+masodo@users.noreply.github.com>
2019-07-01 11:12:20 -0400

    more fluid nav layout

--- ArcadeInfo.php
+++ ArcadeInfo.php
@@ -11,7 +11,7 @@
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: ArcadeInfo.php  Function: Latest Site Info Block   Modified: 6/28/2019   By: MaSoDo
+# Section: ArcadeInfo.php  Function: Latest Site Info Block   Modified: 7/1/2019   By: MaSoDo
 ?>
 <br />
 <?php
@@ -27,14 +27,14 @@ $anysent=mysql_num_rows(run_query("SELECT `status` FROM `PLA_pun_pm_messages` WH
 if($anysent > 0){ $AlertMe='yes'; $alertstyle='border:lime dotted; color:lime;'; }
 }
 ?>
-<table align="center" width="1200" cellpadding="4" cellspacing="1">
+<table align="center" cellpadding="4" cellspacing="1">
 <tr>
 <td colspan="3" class="arcade1" align="center">
 <?php
 if (!isset($_COOKIE['phpqa_user_c'])) {
 // begin nav buttons
 ?>
-<div style="width: 100%; text-align: center; margin-left: auto; margin-right: auto;"><a name="Login"></a><div class="navigation">Logged off: [ <a href="javascript:tog('login_form')">Login</a> ]</div><div class="navigation">[ <a href="index.php?action=register#registration">Register</a> ] </div><div class="navigation"><a href="index.php?action=leaderboards">Leaderboard</a></div><div class="navigation"><a href="index.php?action=HOF">Hall of Fame</a></div><div class="navigation"><a href="javascript:tog('search');">Search</a></div><div class="navigation"><a href="index.php?action=members">Members</a></div><div class="navigation" style="<?php echo $alertstyle; ?>"><a href="/FORUM">Forum (Separate Login)</a></div>
+<div style="text-align: center; margin-left: auto; margin-right: auto;"><a name="Login"></a><div class="navigation">Logged off: [ <a href="javascript:tog('login_form')">Login</a> ]</div><div class="navigation">[ <a href="index.php?action=register#registration">Register</a> ] </div><div class="navigation"><a href="index.php?action=leaderboards">Leaderboard</a></div><div class="navigation"><a href="index.php?action=HOF">Hall of Fame</a></div><div class="navigation"><a href="javascript:tog('search');">Search</a></div><div class="navigation"><a href="index.php?action=members">Members</a></div><div class="navigation" style="<?php echo $alertstyle; ?>"><a href="/FORUM">Forum (Separate Login)</a></div>
 <div style="display:none" id="login_form" name="tog_collect"><br /><br /><br /><form method="post" action="?action=login"> Name: <input type="text" name="userID" style="font-size: 80%;" /> <font style="font-size: 80%;"></font> Pass: <input type="password" name="pword" style="font-size: 80%;" /> <input type="submit" value="Login" style="font-size: 80%;" /><br /><input type="checkbox" name="cookiescheck" />Remember Login? | <a href="index.php?action=register#registration"><b>Register to play!</b></a><br /><a href="index.php?action=forgotpass"><i>Forgot Password?</i></a></form></div></div>
 <?php
 } else {
