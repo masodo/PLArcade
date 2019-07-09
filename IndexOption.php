4be885e36269f188a8edc2a525b5a689c2fd74c1
masodo <41118685+masodo@users.noreply.github.com>
2019-07-09 15:07:36 -0400

    bugfix for remote avatars

--- IndexOption.php
+++ IndexOption.php
@@ -11,7 +11,7 @@
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: IndexOption.php  Function: Display Games Index   Modified: 6/26/2019   By: MaSoDo
+# Section: IndexOption.php  Function: Display Games Index   Modified: 7/9/2019   By: MaSoDo
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	//		  Favorites
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -88,7 +88,7 @@ $DL_action="<a href='GetGame.php?GID=".$g['gameid']."' title='Download Game TAR'
 echo "</div><div class='viewedtimes' style='float: right;'>".$DL_action.$fav_action."</div></td>";
 if ($g['gamecat'] != '20' && $g['gamecat'] != '16') {
 echo "<tr><td align='center' height='20px' class='headertableblock'>Top Score</tr>";
-echo "<tr><td class='arcade1 fheight1' valign='top' align='center'><img alt='image' src='".$crowndir."/crown1.gif' /><br /><b>".str_replace('-', '', $g['Champion_score'])."</b><br /><div style='height:60px;'><img src='".$arcurl."/".$CHMPimg['avatar']."'  height='40' width='40' /><br /><b>".($g['Champion_name']?"<a href='index.php?action=profile&amp;user=".$g['Champion_name']."' class='".$CHMPimg['group']."Look'>".$g['Champion_name']."</a></b>":"------------</b>")."</div><p><a href='index.php?id=".$g['gameid']."'>View Highscores</a></p>";
+echo "<tr><td class='arcade1 fheight1' valign='top' align='center'><img alt='image' src='".$crowndir."/crown1.gif' /><br /><b>".str_replace('-', '', $g['Champion_score'])."</b><br /><div style='height:60px;'><img src='".$CHMPimg['avatar']."'  height='40' width='40' /><br /><b>".($g['Champion_name']?"<a href='index.php?action=profile&amp;user=".$g['Champion_name']."' class='".$CHMPimg['group']."Look'>".$g['Champion_name']."</a></b>":"------------</b>")."</div><p><a href='index.php?id=".$g['gameid']."'>View Highscores</a></p>";
 } else {
 echo "<tr><td align='center' height='20px' class='headertableblock'>No Scores Recorded For This Game</tr>";
 echo "<tr><td class='arcade1 fheight1' valign='top' align='center'><div style='overflow:hidden; height:113px'><img alt='image' src='".$crowndir."/crown1.gif' />&nbsp;&nbsp;<img alt='image' src='".$crowndir."/crown2.gif' />&nbsp;&nbsp;<img alt='image' src='".$crowndir."/crown3.gif' /><br /><br /><i>Sorry, but this game does not record<br />your score in the arcade.</i><br /><b>Please enjoy this selection<br />just for the fun of it!</b><br /><div style='height:56px;'>&nbsp;</div></div>";
