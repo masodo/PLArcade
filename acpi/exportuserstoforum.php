a39a58a010b5d9ac4ad4ec46dcfa8a79b4169205
masodo <41118685+masodo@users.noreply.github.com>
2019-07-23 12:59:38 -0400

    removed back-ticks

--- acpi/exportuserstoforum.php
+++ acpi/exportuserstoforum.php
@@ -1,5 +1,5 @@
 <?php
-$tquery = run_query("select * from `phpqa_accounts`");
+$tquery = run_query("select * from phpqa_accounts");
 while($truser = mysql_fetch_array($tquery)){
 $usegroup = '';
 $takename = $truser['name'];
@@ -12,8 +12,8 @@ if ($takegroup == 'Member')$usegroup = 3;
 if ($takegroup == 'Admin')$usegroup = 1;
 if ($takegroup == 'Moderator')$usegroup = 4;
 if ($takegroup == 'Affiliate')$usegroup = 5;
-$regtime = time();
-run_query("INSERT INTO `PLA_users` VALUES('', ".$usegroup.", '".$takename."', '".$takepass."', NULL, '".$takeemail."', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 'English', 'Oxygen', 1, 0, NULL, NULL, '".$regtime."', '".$takeIP."', 0, NULL, NULL, NULL, 0, 0, 0);");
+
+run_query("INSERT INTO PLA_users VALUES('', ".$usegroup.", '".$takename."', '".$takepass."', NULL, '".$takeemail."', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 'English', 'Oxygen', 1, 0, NULL, NULL, 0, '".$takeIP."', 0, NULL, NULL, NULL, 0, 0, 0, NULL, 1);");
 
 }
 echo "<script>alert('DONE!')</script>";
