7a09a168aade5e73e426044547ad2443ef46e0af
masodo <41118685+masodo@users.noreply.github.com>
2019-07-23 10:27:33 -0400

    include timestamp

--- acpi/exportuserstoforum.php
+++ acpi/exportuserstoforum.php
@@ -12,8 +12,8 @@ if ($takegroup == 'Member')$usegroup = 3;
 if ($takegroup == 'Admin')$usegroup = 1;
 if ($takegroup == 'Moderator')$usegroup = 4;
 if ($takegroup == 'Affiliate')$usegroup = 5;
-
-run_query("INSERT INTO `PLA_users` VALUES('', ".$usegroup.", '".$takename."', '".$takepass."', NULL, '".$takeemail."', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 'English', 'Oxygen', 1, 0, NULL, NULL, 0, '".$takeIP."', 0, NULL, NULL, NULL, 0, 0, 0);");
+$regtime = time();
+run_query("INSERT INTO `PLA_users` VALUES('', ".$usegroup.", '".$takename."', '".$takepass."', NULL, '".$takeemail."', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 'English', 'Oxygen', 1, 0, NULL, NULL, '".$regtime."', '".$takeIP."', 0, NULL, NULL, NULL, 0, 0, 0);");
 
 }
 echo "<script>alert('DONE!')</script>";
