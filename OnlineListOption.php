18da0ccf711693aa4cdadc1e42e7709c3f373aff
masodo <41118685+masodo@users.noreply.github.com>
2019-07-26 12:31:30 -0400

    Incompatible Code Commented

--- OnlineListOption.php
+++ OnlineListOption.php
@@ -1,6 +1,6 @@
 <?php
 //-----------------------------------------------------------------------------------/
-//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 © Jcink.com
+//Practical-Lightning-Arcade [PLA] 2.0 (ALPHA) based on PHP-Quick-Arcade 3.0 © Jcink.com
 //Tournaments & JS By: SeanJ. - Heavily Modified by PracticalLightning Web Design
 //Michael S. DeBurger [DeBurger Photo Image & Design]
 //-----------------------------------------------------------------------------------/
@@ -11,7 +11,7 @@
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: OnlineListOption.php  Function: Maintain Online User List   Modified: 6/17/2019   By: MaSoDo
+# Section: OnlineListOption.php  Function: Maintain Online User List   Modified: 7/26/2019   By: MaSoDo
 
 			// = = = = = = = = = = = = = = = = = = 
 			// 		Online List
@@ -29,6 +29,9 @@ if(isset($_GET['action']) && $_GET['action'] == "profile") $w="Viewing A Member 
 if(isset($_GET['action']) && $_GET['action'] == "leaderboards") $w="Viewing The Leaderboard";
 if(isset($_GET['action']) && $_GET['action'] == "members") $w="Viewing The Members List";
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Incompatible Function Block #1
 $areyouthere=@mysql_fetch_array(run_query("SELECT name FROM phpqa_sessions WHERE name='".$phpqa_user_cookie."'"));
 if(!$areyouthere) {
 global $w; 
@@ -37,6 +40,9 @@ run_query("INSERT INTO phpqa_sessions (name,time,location) VALUES ('$phpqa_user_
 global $w; 
 $areyouthere=run_query("UPDATE `phpqa_sessions` SET `time` = '$time', `location` = '$w' WHERE name='$phpqa_user_cookie'");
 }
+//END Incompatible Function Block #1
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 // End Collapse #4
 echo "</div>";
 } else { 
@@ -44,6 +50,9 @@ echo "</div>";
 }
 if(!isset($_GET['id']) && !isset($_GET['cparea']) && !isset($_GET['modcparea']) && !isset($_GET['play'])) $w="Viewing Arcade Index"; 
 echo "<div class='tableborder'><table width='100%' cellpadding='4' cellspacing='1'><tr><td class='arcade1' align='left'><fieldset class=\"search\"><legend>Users Online in the Past ".$settings['online_list_dur']." Minutes: (<a href='./index.php?action=Online&method=time' target='_top'>Last Click</a>, <a href='./index.php?action=Online&method=name' target='_top'>Member Name</a>) <span class='adminLook'>Administrator</span>  <span class='moderatorLook'>Moderator</span>  <span class='memberLook'>Member</span>  <span class='affiliateLook'>Affiliate</span> </legend><br />";
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Incompatible Function Block #2
 $online=run_query("SELECT * FROM phpqa_sessions ORDER by time DESC");
 	while($g=mysql_fetch_array($online)){ 
 	$onnow = run_query("SELECT `group` FROM `phpqa_accounts` WHERE `name` = '" . $g['name'] . "'");
@@ -54,6 +63,9 @@ $HowManyMinutes=round(abs($lasttime-$time) / 60,2);
 echo "</div>";
 if($HowManyMinutes > $settings['online_list_dur']) { 
 run_query("DELETE FROM phpqa_sessions WHERE name='".$g['name']."'");
+//END Incompatible Function Block #2
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 } else {
 $where=($g['location']);
 if ($where=='')$where="Viewing Arcade Index";
