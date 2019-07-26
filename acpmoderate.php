9d5462f56dde1f0ccc1fd32206197186b65d9c4c
masodo <41118685+masodo@users.noreply.github.com>
2019-07-26 12:48:15 -0400

    Incompatible Code Commented

--- acpmoderate.php
+++ acpmoderate.php
@@ -1,6 +1,6 @@
 <?php
 //-----------------------------------------------------------------------------------/
-//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 © Jcink.com
+//Practical-Lightning-Arcade [PLA] 2.0 (ALPHA) based on PHP-Quick-Arcade 3.0 © Jcink.com
 //Tournaments & JS By: SeanJ. - Heavily Modified by PracticalLightning Web Design
 //Michael S. DeBurger [DeBurger Photo Image & Design]
 //-----------------------------------------------------------------------------------/
@@ -11,7 +11,7 @@
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: acpmoderate.php  Function: Moderator Control Panel   Modified: 3/11/2019   By: MaSoDo
+# Section: acpmoderate.php  Function: Moderator Control Panel   Modified: 7/26/2019   By: MaSoDo
 if(isset($_REQUEST['modcpcheck'])) {
 echo "<script type='text/javascript'>alert('Die Request')</script>"; 
 die();
@@ -30,7 +30,13 @@ if(isset($_GET['shoutdel'])) {
 $shoutnumber = $_GET['shoutdel'];
 if(is_numeric($shoutnumber)) {
 vsess();
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Incompatible Function Block #1
 run_query("DELETE FROM `phpqa_shoutbox` WHERE `id` = '".$shoutnumber."'");
+//END Incompatible Function Block #1
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 }
 }
 if (isset($_POST['dowhat_m'])&&$_POST['dowhat_m']=='comment') {
@@ -38,7 +44,12 @@ global $score_m;
 for($x=0;$x<=count($score_m)-1;$x++){
 if(!is_numeric($score_m[$x])) die();
 vsess();
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Incompatible Function Block #2
 run_query("UPDATE `phpqa_scores` SET `comment` = '' WHERE id='".$score_m[$x]."'");
+//END Incompatible Function Block #2
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 }
 }
 if (isset($_POST['dowhat_m'])&&$_POST['dowhat_m']=='erase') {
@@ -48,6 +59,9 @@ for($x=0;$x<=count($score_m)-1;$x++){
 if(!is_numeric($score_m[$x])) die();
 $id = htmlspecialchars($_GET['id'], ENT_QUOTES);
 // Is this person a champ?
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Incompatible Function Block #3
 $whoischamp = mysql_fetch_array(run_query("SELECT `Champion_name`,`Champion_score` FROM `phpqa_games` WHERE `gameid` = '".$id."'"));
 $score_being_deleted = mysql_fetch_array(run_query("SELECT `username`,`thescore` FROM `phpqa_scores` WHERE `id` = '".$score_m[$x]."'"));
 	// Erase them from the scoreboards, the games, and leaderboard for this game 	if thats true.
@@ -70,6 +84,9 @@ run_query("INSERT INTO `phpqa_leaderboard` (`username`, `thescore`,`gamename`) V
 }
 }
 }
+//END Incompatible Function Block #3
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 if(isset($_GET['modcparea'])) {
 // =================================== \\
 // 		Visuals
@@ -99,8 +116,14 @@ $choice = htmlspecialchars(isset($_POST['choice']), ENT_QUOTES);
 $choice = str_replace(" ", "___", $choice);
 $tool=htmlspecialchars(isset($_POST['tool']), ENT_QUOTES);
 if(isset($_POST['tool'])) {
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Incompatible Function Block #4
 $selectfrom=run_query("SELECT * FROM phpqa_scores WHERE ".$choice."='".$tool."' ORDER BY phpdate DESC");
 	while($g=mysql_fetch_array($selectfrom)){ 
+//END Incompatible Function Block #4
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
 $parse_stamp = date($datestamp, "".$g[5]."");
 echo "<div class='tableborder'><table width='100%' cellpadding='5' cellspacing='1' class='highscore'><tr><td width='2%' class='headertableblock' align='center'>Username</td><td width='15%' class='headertableblock' align='center'>Score</td><td width='30%' class='headertableblock' align='center'>Comments</td><td width='30%' class='headertableblock' align='center'>Time &amp; Date</td><td width='20%' class='headertableblock' align='center'>IP Address</td><td width='10%' class='headertableblock' align='center'>ScoreBoard</td>";
 echo "<tr><td class='arcade1' align='center'><a href='index.php?action=profile&amp;user=".$g[1]."'>".$g[1]."</a></td><td class='arcade1' align='center'>".$g[2]."</td><td class='arcade1' width='40%' align='center'>".$g['comment']."</td><td class='arcade1' width='20%' align='center'>".$parse_stamp."</td>";
