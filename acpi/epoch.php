779fc62300bb5e426a34c12784c982103d7e530f
masodo <41118685+masodo@users.noreply.github.com>
2019-07-26 13:48:24 -0400

    PHP7+ Compatible

--- acpi/epoch.php
+++ acpi/epoch.php
@@ -1,17 +1,17 @@
 <?php
 //-----------------------------------------------------------------------------------/
-//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 © Jcink.com
+//Practical-Lightning-Arcade [PLA] 2.0 (ALPHA) based on PHP-Quick-Arcade 3.0 Â© Jcink.com
 //Tournaments & JS By: SeanJ. - Heavily Modified by PracticalLightning Web Design
 //Michael S. DeBurger [DeBurger Photo Image & Design]
 //-----------------------------------------------------------------------------------/
-//  phpQuickArcade v3.0.x © Jcink 2005-2010 quickarcade.jcink.com                        
+//  phpQuickArcade v3.0.x Â© Jcink 2005-2010 quickarcade.jcink.com                        
 //
 //  Version: 3.0.23 Final. Released: Sunday, May 02, 2010
 //-----------------------------------------------------------------------------------/
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: acpi Place: epoch - Administrator Control Panel   Modified: 2/28/2019   By: MaSoDo
+# Section: acpi Place: epoch - Administrator Control Panel   Modified: 7/26/2019   By: MaSoDo
 $epoch = '';
 $showepoch = time();
 if (isset($_GET['hh'])) {
@@ -44,4 +44,3 @@ $epoch = mktime ( $hour, $minute, $second, $month, $day, $year );
 <div style="color: green; margin-bottom: 15px; font-size:12px;">Requested EPOCH STAMP:<br /><span style="font-size:42px;"><?php echo($epoch); ?></span></div>
 </div>
 </form>
-
