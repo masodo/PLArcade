5a870871d3d7832c497fd2e7e42c7b478b2eb652
masodo <41118685+masodo@users.noreply.github.com>
2019-07-26 12:19:57 -0400

    Incompatible Code Commented

--- Afiliates.php
+++ Afiliates.php
@@ -1,6 +1,6 @@
 <?php
 //-----------------------------------------------------------------------------------/
-//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 © Jcink.com
+//Practical-Lightning-Arcade [PLA] 2.0 (ALPHA) based on PHP-Quick-Arcade 3.0 © Jcink.com
 //Tournaments & JS By: SeanJ. - Heavily Modified by PracticalLightning Web Design
 //Michael S. DeBurger [DeBurger Photo Image & Design]
 //-----------------------------------------------------------------------------------/
@@ -11,14 +11,19 @@
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: Afiliates.php  Function: Scrolling Afiliate Banner   Modified: 7/19/2019   By: MaSoDo
+# Section: Afiliates.php  Function: Scrolling Afiliate Banner   Modified: 7/26/2019   By: MaSoDo
 
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Incompatible Function Block #1
 $getaffil = run_query("SELECT * FROM `phpqa_affiliate` ORDER BY `sort` ASC");
 echo '<div style="width:80%; height:85px; background-color:black; border:inset; margin-left: auto; margin-right:auto; padding-top: 5px; margin-bottom:25px; color:navy;">';
 echo '<marquee BEHAVIOR="ALTERNATE">';
 
 while($useaffil = mysql_fetch_array($getaffil)) {
+//END Incompatible Function Block #1
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 if ($useaffil['active'] == 1){
 echo "<span style='color:silver; font-size: 30px;'> &bull;  &bull;  &bull; </span>";
 echo "<a href='".$useaffil['url']."' title='".$useaffil['tag']."' target='_blank'><img src='".$arcurl."/".$bannerloc."/".$useaffil['img']."' border='0' width='400' height='75' /></a>";
