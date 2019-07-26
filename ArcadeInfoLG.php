235b90dea5f5998108aa60d903d0746d9cdc8bc6
masodo <41118685+masodo@users.noreply.github.com>
2019-07-26 09:49:50 -0400

    Incompatible Code Commented

--- ArcadeInfoLG.php
+++ ArcadeInfoLG.php
@@ -1,6 +1,6 @@
 <?php
 //-----------------------------------------------------------------------------------/
-//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 © Jcink.com
+//Practical-Lightning-Arcade [PLA] 2.0 (ALPHA) based on PHP-Quick-Arcade 3.0 © Jcink.com
 //Tournaments & JS By: SeanJ. - Heavily Modified by PracticalLightning Web Design
 //Michael S. DeBurger [DeBurger Photo Image & Design]
 //-----------------------------------------------------------------------------------/
@@ -11,7 +11,7 @@
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: ArcadeInfoLG.php By: Legionaire Function: Latest Site Info Block   Modified: 7/23/2019   By: MaSoDo
+# Section: ArcadeInfoLG.php By: Legionaire Function: Latest Site Info Block   Modified: 7/26/2019   By: MaSoDo
 ?>
 <br />
 <?php
@@ -22,6 +22,9 @@ $alertlink = '';
 $forumnote = '';
 if (isset($_COOKIE['phpqa_user_c'])) {
 $MyMess = $_COOKIE['phpqa_user_c'];
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Incompatible Function Block #1
 $checkmessages=run_query("SELECT `id` FROM `PLA_users` where `username` = '".$MyMess."'"); 
 $checkmess= mysql_fetch_array($checkmessages);
 $checkmess_for = $checkmess['id'];
@@ -35,6 +38,9 @@ $anyOsent=mysql_num_rows(run_query("SELECT `status` FROM `PLA_pun_pm_messages` W
 if($anyOsent > 0){ $AlertMe='yes'; $alertstyle='border:solid orange;'; $alertlink='color:orange;'; $forumnote=' (<b>PM</b> alt)'; }
 }
 }
+//END Incompatible Function Block #1
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 ?>
 <table align="center" width="1200" cellpadding="4" cellspacing="1">
 <tr>
@@ -47,8 +53,14 @@ if (!isset($_COOKIE['phpqa_user_c'])) {
 <div style="display:none" id="login_form" name="tog_collect"><br /><br /><br /><form method="post" action="?action=login"> Name: <input type="text" name="userID" style="font-size: 80%;" /> <font style="font-size: 80%;"></font> Pass: <input type="password" name="pword" style="font-size: 80%;" /> <input type="submit" value="Login" style="font-size: 80%;" /><br /><input type="checkbox" name="cookiescheck" />Remember Login? | <a href="index.php?action=register#registration"><b>Register to play!</b></a><br /><a href="index.php?action=forgotpass"><i>Forgot Password?</i></a></form></div></div>
 <?php
 } else {
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Incompatible Function Block #2
 $checkChamps = mysql_num_rows(run_query("SELECT username FROM phpqa_leaderboard WHERE username = '$phpqa_user_cookie'"));
 echo "<div style='width: 100%; text-align: center; margin-left: auto; margin-right: auto;'><div  class='navigation'>Logged in: <A href='index.php?action=profile&amp;user=$phpqa_user_cookie'>$phpqa_user_cookie</a> Total Wins: $checkChamps </div><div class='navigation'><a href='index.php?action=settings'>Settings</a>";
+//END Incompatible Function Block #2
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 if ($exist[6]=="Admin") {
 echo " (<a href='index.php?cpiarea=idx'><b>Admin CP</b></a>) &middot; (<a href='index.php?modcparea=idx'><b>Mod CP</b></a>)";
 }
@@ -61,7 +73,15 @@ echo"</div><div  class='navigation'>[ <a href='index.php?action=logout'>Log Out<
 ?>
 </td>
 </tr></table>
-<div style="display:none; width:250px; margin-left:auto; margin-right:auto;" class='tableborder' id="search" name="tog_collect"><br />Search The Arcade<br /><br /><form method="get">Term: <input type="text" name="search" value="" /><br />Search By: <select name="by"><?php foreach(Array('game'=>'Game Name','gameid'=>'Game ID','about'=>'Game desc','Champion_name'=>'Champion Name') as $k=>$v) echo "<option value='$k'>$v</option>";?></select><br />In Category: <select size="1" name="searchcat"><option value='All' selected="selected">All</option><?php $catquery=run_query("SELECT * FROM phpqa_cats ORDER BY `displayorder`");while ($catlist= mysql_fetch_array($catquery)) echo  "<option value='$catlist[0]'>$catlist[1]</option>"; ?></select><br /><input type="submit" value="search" name="action" /></form></div>
+<div style="display:none; width:250px; margin-left:auto; margin-right:auto;" class='tableborder' id="search" name="tog_collect"><br />Search The Arcade<br /><br /><form method="get">Term: <input type="text" name="search" value="" /><br />Search By: <select name="by"><?php foreach(Array('game'=>'Game Name','gameid'=>'Game ID','about'=>'Game desc','Champion_name'=>'Champion Name') as $k=>$v) echo "<option value='$k'>$v</option>";?></select><br />In Category: <select size="1" name="searchcat"><option value='All' selected="selected">All</option><?php 
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Incompatible Function Block #3
+$catquery=run_query("SELECT * FROM phpqa_cats ORDER BY `displayorder`");while ($catlist= mysql_fetch_array($catquery)) echo  "<option value='$catlist[0]'>$catlist[1]</option>"; 
+//END Incompatible Function Block #3
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+?></select><br /><input type="submit" value="search" name="action" /></form></div>
 <div align="center">
 <?php // Begin Collapse #1 
 if (isset($settings["show_stats_table"])&&$settings["show_stats_table"]=='1') { 
@@ -105,10 +125,15 @@ if (!isset($acct_setting[3]) || $acct_setting[3] !="No") {
 ?>
 <td class="arcade1" valign="top" align="left">
 <?php
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Incompatible Function Block #4
 $newgames = run_query("SELECT `gameid`,`game`,`id`,`gamecat` FROM `phpqa_games` ORDER by `id` DESC LIMIT 0,".$ngnum."");
 	while($g=mysql_fetch_array($newgames)){ 
 	if (($g[3] != '23')&&($g[3] != '2')){
-	
+//END Incompatible Function Block #4
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 echo "<img height='20' width='20' src='arcade/pics/$g[0].gif' alt='$g[1]' /><a href=\"index.php?play=$g[0]#playzone\">$g[1]</a><br />";
 }}
 ?>
@@ -117,6 +142,9 @@ echo "<img height='20' width='20' src='arcade/pics/$g[0].gif' alt='$g[1]' /><a h
 <td class="headerblock" valign="top" nowrap="nowrap" align="center">
 <table width="100%"><td class="arcade1" valign="top" align="left">
 <?php
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Incompatible Function Block #4
 $scoreboard = run_query("SELECT phpqa_accounts.name,phpqa_accounts.avatar,phpqa_accounts.group, COUNT(phpqa_leaderboard.username) AS champions FROM phpqa_accounts
 LEFT JOIN phpqa_leaderboard ON phpqa_accounts.name = phpqa_leaderboard.username
 GROUP BY phpqa_leaderboard.username
@@ -136,7 +164,9 @@ if ($trop == 3) { $trophy = "<img src='$crowndir/crown3.gif' style='margin-left:
 if ($scores['avatar'] == ''){ $scores['avatar'] = $avatarloc.'/man.gif'; }
 echo "<td class='arcade1'><div align='center'><i><span style='font-size: 14px; line-height:175%'><a href=\"index.php?action=profile&amp;user=".$scores['name']."\"><img src='".$scores['avatar']."' height='60px' /><br />" . $trophy . "<span class='".$scores['group']."Look'>".$scores['name']."</span>" . $trophy . "</a></i><br /><div padding:3px; background-color:navy;' align='center'>with <b>".$scores['champions']."</b> wins!</div></div></td>";
 }
- // end play check
+//END Incompatible Function Block #4
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 ?>
 </td>
 </tr>
@@ -149,6 +179,8 @@ echo "<td class='arcade1'><div align='center'><i><span style='font-size: 14px; l
 <?php
 echo "";
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Incompatible Function Block #5
 	$selectfrom = run_query("SELECT * FROM `phpqa_scores` ORDER BY `phpdate` DESC LIMIT 0,$lsnum");
 	while($s=mysql_fetch_array($selectfrom)){ 
   $VstatG = "";
@@ -168,17 +200,27 @@ $thisGroup = mysql_fetch_array($findGroup);
 $parse_stamp = date($datestamp, $s[5]);
 echo "$VstatG<a href='index.php?action=profile&amp;user=".$s[1]."' class='".$thisGroup[0]."Look'>".$s[1]."</a>" . $bigtag . " scored <i>" . str_replace('-', '', $s[2]) . "</i> in <a href='index.php?id=".$s[6]."#playzone'><i>$s[7]</i></a> on ".$parse_stamp."<hr>";
 }
+//END Incompatible Function Block #5
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 ?>
 </td><td class="arcade1" valign="top" align="left">
 <?php
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Incompatible Function Block #6
 $hotgames = run_query("SELECT `gameid`,`game`,`id`,`gamecat`,`times_played`  FROM `phpqa_games` ORDER by `times_played` DESC LIMIT $lsnum");
 	while($hg=mysql_fetch_array($hotgames)){ 
 	if ($hg[3] != '2') {
 echo "<img height='20' width='20' src='arcade/pics/$hg[0].gif' alt='$hg[1]' style='margin-left:5px;' /><a href=\"index.php?play=$hg[0]#playzone\">$hg[1]</a> (".$hg['times_played'].")<br />";
 }}
+//END Incompatible Function Block #6
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 } // End acct based check for big table
 } // End check for big table
-}
+} // end play check
 ?>
 </td></tr></table>
 </table></td>
