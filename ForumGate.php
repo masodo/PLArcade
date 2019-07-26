dc3b1426324fd6b97fcd6118b3c74d78d10370ea
masodo <41118685+masodo@users.noreply.github.com>
2019-07-26 11:16:01 -0400

    Incompatible Code Commented

--- ForumGate.php
+++ ForumGate.php
@@ -1,22 +1,25 @@
 <?php
 //-----------------------------------------------------------------------------------/
-//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 © Jcink.com
+//Practical-Lightning-Arcade [PLA] 2.0 (ALPHA) based on PHP-Quick-Arcade 3.0 Â© Jcink.com
 //Tournaments & JS By: SeanJ. - Heavily Modified by PracticalLightning Web Design
 //Michael S. DeBurger [DeBurger Photo Image & Design]
 //-----------------------------------------------------------------------------------/
-//  phpQuickArcade v3.0.x © Jcink 2005-2010 quickarcade.jcink.com                        
+//  phpQuickArcade v3.0.x Â© Jcink 2005-2010 quickarcade.jcink.com                        
 //
 //  Version: 3.0.23 Final. Released: Sunday, May 02, 2010
 //-----------------------------------------------------------------------------------/
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: ForumGate.php Function: GoTo Forum   Modified: 7/1/2019   By: MaSoDo
+# Section: ForumGate.php Function: GoTo Forum   Modified: 7/26/2019   By: MaSoDo
 
 $time=time();
 if (isset($_COOKIE['phpqa_user_c'])) {
 $phpqa_user_cookie = $_COOKIE['phpqa_user_c'];
 $w="Visiting the Forums";}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Incompatible Function Block #1
 $areyouthere=@mysql_fetch_array(run_query("SELECT name FROM phpqa_sessions WHERE name='".$phpqa_user_cookie."'"));
 if(!$areyouthere) {
 global $w; 
@@ -25,5 +28,7 @@ run_query("INSERT INTO phpqa_sessions (name,time,location) VALUES ('$phpqa_user_
 global $w; 
 $areyouthere=run_query("UPDATE `phpqa_sessions` SET `time` = '$time', `location` = '$w' WHERE name='$phpqa_user_cookie'");
 }
+//END Incompatible Function Block #1
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 header("Location: /FORUM/");
 ?>
