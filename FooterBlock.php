87cabd6556ebeb369c7fffb04ae51a89997df4df
masodo <41118685+masodo@users.noreply.github.com>
2019-07-26 15:43:04 -0400

    PHP7+ Compatible - Converted

--- FooterBlock.php
+++ FooterBlock.php
@@ -1,6 +1,6 @@
 <?php
 //-----------------------------------------------------------------------------------/
-//Practical-Lightning-Arcade [PLA] 1.0 (BETA) based on PHP-Quick-Arcade 3.0 © Jcink.com
+//Practical-Lightning-Arcade [PLA] 2.0 (ALPHA) based on PHP-Quick-Arcade 3.0 © Jcink.com
 //Tournaments & JS By: SeanJ. - Heavily Modified by PracticalLightning Web Design
 //Michael S. DeBurger [DeBurger Photo Image & Design]
 //-----------------------------------------------------------------------------------/
@@ -11,19 +11,19 @@
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: FooterBlock.php  Function: Bottom of Pages   Modified: 6/21/2019   By: MaSoDo
+# Section: FooterBlock.php  Function: Bottom of Pages   Modified: 7/26/2019   By: MaSoDo
 if(isset($settings['closed_arcade']) && $settings['closed_arcade'] == 1 && !isset($_COOKIE['phpqa_user_c'])){}else{
 if(isset($settings['enable_onlinelist'])) {
 require "./OnlineListOption.php";
 }}
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Incompatible Function Block #1
+//Updated Function Block #1
 ?>
-<div class='tableborder'><table width='100%' cellpadding='4' cellspacing='1'><tr><td class='arcade1' style="background-color:gray;"><?php echo date("md")=="0401"?"Gilligans Arcade":"<a href='https://github.com/masodo/PLArcade' target='_blank' style='color:white;'><b>Practical-Lightning-Arcade [PLA] 1.0 </b> (BETA)</a>"; ?>  by <font size="2" face="Lucida Console"><b><a href="http://www.practicallightning.com" target="_blank">practical<sub><img src="<?php echo $imgloc; ?>/PL_logo_sm.gif" alt="PracticalLightning.com" border="0"></sub>lightning</a></b></font> Web Design [<i><a title='DeBurger Photo Image &amp; Design'>DPI&ampD</a></i>]</td><td class='arcade1' style='background-color:gray;color: white'><?php $mtime2=explode(" ",microtime()); echo "Execution Time: ".($mtime2[1]-$mtime[1]).substr($mtime2[0]-$mtime[0],1)."</td><td class='arcade1' style='background-color:gray;color: white'>Queries Used: ".count(run_query());?></td></tr>
+<div class='tableborder'><table width='100%' cellpadding='4' cellspacing='1'><tr><td class='arcade1' style="background-color:gray;"><?php echo date("md")=="0401"?"Gilligans Arcade":"<a href='https://github.com/masodo/PLArcade' target='_blank' style='color:white;'><b>Practical-Lightning-Arcade [PLA] 1.0 </b> (BETA)</a>"; ?>  by <font size="2" face="Lucida Console"><b><a href="http://www.practicallightning.com" target="_blank">practical<sub><img src="<?php echo $imgloc; ?>/PL_logo_sm.gif" alt="PracticalLightning.com" border="0"></sub>lightning</a></b></font> Web Design [<i><a title='DeBurger Photo Image &amp; Design'>DPI&ampD</a></i>]</td><td class='arcade1' style='background-color:gray;color: white'><?php $mtime2=explode(" ",microtime()); echo "Execution Time: ".($mtime2[1]-$mtime[1]).substr($mtime2[0]-$mtime[0],1)."</td><td class='arcade1' style='background-color:gray;color: white'>Queries Used: ".count(run_iquery());?></td></tr>
 <tr><td colspan="3"><div style="text-align:center;"><a href="index.php?contact">Contact</a> | <a href="index.php?privacy">Privacy Policy</a> | <a href="index.php?disclosure">Legal Disclosure</a></div></td><tr></table></div></div><br />
 <?php
-//END Incompatible Function Block #1
+//END Updated Function Block #1
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 include "Afiliates.php";
