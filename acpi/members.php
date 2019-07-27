74250ce97a0f6dfbd0dcbf4370e616cbd1012c2f
masodo <41118685+masodo@users.noreply.github.com>
2019-07-27 10:25:16 -0400

    PHP7+ Compatible - Converted

--- acpi/members.php
+++ acpi/members.php
@@ -11,7 +11,7 @@
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: acpi Place: members - Control Panel   Modified: 7/26/2019   By: MaSoDo
+# Section: acpi Place: members - Control Panel   Modified: 7/27/2019   By: MaSoDo
 //require('exportuserstoforum.php');
 {
 message("View Only: <br /><a href='?cpiarea=members&act=Admin'>Admins</a> &middot; <a href='?cpiarea=members&act=Moderator'>Moderators</a> &middot; <a href='?cpiarea=members&act=Affiliate'>Affiliate</a> &middot; <a href='?cpiarea=members&act=Member'>Members</a> &middot; <a href='?cpiarea=members&act=Banned'>Banned</a> &middot; <a href='?cpiarea=members&act=Validating'>Validating</a>");
@@ -22,16 +22,16 @@ for($x=0;$x<=count($gselect)-1;$x++){
 $f=htmlspecialchars($gselect[$x], ENT_QUOTES);
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Incompatible Function Block #1
-run_query("DELETE FROM `phpqa_scores` WHERE `username`='".$f."'");
-run_query("DELETE FROM `phpqa_leaderboard` WHERE `username`='".$f."'");
-run_query("DELETE FROM `phpqa_accounts` WHERE `name`='".$f."'");
-run_query("UPDATE `phpqa_games` SET `Champion_score` = '' WHERE `Champion_name`='".$f."'");
-run_query("UPDATE `phpqa_games` SET `Champion_name` = '' WHERE `Champion_name`='".$f."'");
-run_query("UPDATE `phpqa_games` SET `HOF_score` = '' WHERE `HOF_name`='".$f."'");
-run_query("UPDATE `phpqa_games` SET `HOF_name` = '' WHERE `HOF_name`='".$f."'");
-run_query("DELETE FROM `phpqa_leaderboard` WHERE `username`='".$f."'");
-//END Incompatible Function Block #1
+//UpdatedFunction Block #1
+run_iquery("DELETE FROM phpqa_scores WHERE username='".$f."'");
+run_iquery("DELETE FROM phpqa_leaderboard WHERE username='".$f."'");
+run_iquery("DELETE FROM phpqa_accounts WHERE name='".$f."'");
+run_iquery("UPDATE phpqa_games SET Champion_score = '' WHERE Champion_name='".$f."'");
+run_iquery("UPDATE phpqa_games SET Champion_name = '' WHERE Champion_name='".$f."'");
+run_iquery("UPDATE phpqa_games SET HOF_score = '' WHERE HOF_name='".$f."'");
+run_iquery("UPDATE phpqa_games SET HOF_name = '' WHERE HOF_name='".$f."'");
+run_iquery("DELETE FROM phpqa_leaderboard WHERE username='".$f."'");
+//END UpdatedFunction Block #1
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 }
@@ -46,18 +46,18 @@ $new_name=htmlspecialchars($_POST['new_name'], ENT_QUOTES);
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Incompatible Function Block #2
-$checkexistance = mysql_fetch_array(run_query("SELECT name FROM phpqa_accounts WHERE name = '".$new_name."'"));
+//UpdatedFunction Block #2
+$checkexistance = mysqli_fetch_array(run_iquery("SELECT name FROM phpqa_accounts WHERE name = '".$new_name."'"));
 if($checkexistance) { 
 message("The name, &quot;".$new_name."&quot; is already being used by another person."); 
 } else {
-run_query("UPDATE `phpqa_games` SET `Champion_name` = '".$new_name."' WHERE `Champion_name`='".$n."'");
-run_query("UPDATE `phpqa_games` SET `HOF_name` = '".$new_name."' WHERE `HOF_name`='".$n."'");
-run_query("UPDATE `phpqa_leaderboard` SET `username` = '".$new_name."' WHERE `username`='".$n."'");
-run_query("UPDATE `phpqa_scores` SET `username` = '".$new_name."' WHERE `username`='".$n."'");
-run_query("UPDATE `phpqa_accounts` SET `name` = '".$new_name."' WHERE `name`='".$n."'");
+run_iquery("UPDATE phpqa_games SET Champion_name = '".$new_name."' WHERE Champion_name='".$n."'");
+run_iquery("UPDATE phpqa_games SET HOF_name = '".$new_name."' WHERE HOF_name='".$n."'");
+run_iquery("UPDATE phpqa_leaderboard SET username = '".$new_name."' WHERE username='".$n."'");
+run_iquery("UPDATE phpqa_scores SET username = '".$new_name."' WHERE username='".$n."'");
+run_iquery("UPDATE phpqa_accounts SET name = '".$new_name."' WHERE name='".$n."'");
 }
-//END Incompatible Function Block #2
+//END UpdatedFunction Block #2
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 }
@@ -81,25 +81,25 @@ $n=htmlspecialchars($_GET['changepwd'], ENT_QUOTES);
 vsess();
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Incompatible Function Block #3
-run_query("UPDATE `phpqa_accounts` SET `pass` = '".$new_pass."' WHERE `name`='".$n."'", 1); 
-run_query("UPDATE `PLA_users` SET `password` = '".$new_pass."' WHERE `username`='".$n."'", 1);
+//UpdatedFunction Block #3
+run_iquery("UPDATE phpqa_accounts SET pass = '".$new_pass."' WHERE name='".$n."'", 1); 
+run_iquery("UPDATE PLA_users SET password = '".$new_pass."' WHERE username='".$n."'", 1);
 }}
 if(isset($_GET['validate'])) { vsess();
 	$n=htmlspecialchars($_GET['validate'], ENT_QUOTES);
-	run_query("UPDATE `phpqa_accounts` SET `group` = 'Member' WHERE name='".$n."'");
+	run_iquery("UPDATE phpqa_accounts SET group = 'Member' WHERE name='".$n."'");
 }
 
 if(isset($_GET['deleteav'])) { 
 $a=htmlspecialchars($_GET['deleteav'], ENT_QUOTES);
 	vsess();
-	run_query("UPDATE `phpqa_accounts` SET `avatar` = '' WHERE name='".$a."'"); }
+	run_iquery("UPDATE phpqa_accounts SET avatar = '' WHERE name='".$a."'"); }
 if(isset($_GET['changegroupgo'])) {
 $cg=htmlspecialchars($_GET['changegroupgo'], ENT_QUOTES);
 $fg=htmlspecialchars($_POST['chosengroup'], ENT_QUOTES);
 	vsess();
-	run_query("UPDATE `phpqa_accounts` SET `group` = '".$fg."' WHERE name='".$cg."'"); }
-//END Incompatible Function Block #3
+	run_iquery("UPDATE phpqa_accounts SET `group` = '".$fg."' WHERE name='".$cg."'"); }
+//END UpdatedFunction Block #3
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	
 if (isset($_GET['changegroup'])) {
@@ -118,17 +118,17 @@ About: <a href='javascript:alert("As an admin, a user has FULL control of the ar
 <?php
 }
 if(!isset($_GET['act'])) { 
-$memberdata=run_query("SELECT * FROM phpqa_accounts"); 
+$memberdata=run_iquery("SELECT * FROM phpqa_accounts"); 
 } else {
 $picked=htmlspecialchars($_GET['act'], ENT_QUOTES);
-$memberdata=run_query("SELECT * FROM phpqa_accounts WHERE `group`='$picked'"); 
+$memberdata=run_iquery("SELECT * FROM phpqa_accounts WHERE `group`='".$picked."'"); 
 }
 ?>
 <form action='' method='POST'>
 <?php echo "<input type='hidden' name='akey' value='$key'>"; ?>
 <div class='tableborder'><table width=100% cellpadding='4' cellspacing='1'><td width=30% align=center class=headertableblock>Name</td><td width=70% align=center class=headertableblock>Edit</td><td width=20% align=center class=headertableblock>Email</td><td width=20% align=center class=headertableblock>IP</td><td width=60% align=center class=headertableblock><input type='checkbox' onclick="s=document.getElementsByTagName('input');for(x=0;x<s.length;x++) if (s[x].type=='checkbox') s[x].checked=this.checked" /></td>
 <?php
-	while($g=mysql_fetch_array($memberdata)){ 
+	while($g=mysqli_fetch_array($memberdata)){ 
 ?>
 <tr><td class=arcade1 alighn='left'><?php echo $g[1]; ?> (<i><?php echo $g['group']; ?></i>)
 </td><td class=arcade1><div align=center><A href='?cpiarea=members&changegroup=<?php echo $g[1]; ?>'>[ Edit Group ]</a> <A href='?cpiarea=members&deleteav=<?php echo $g[1]; ?>&akey=<?php echo $key; ?>'>[ Delete Avatar ]</a> <A href='?cpiarea=members&changepass=<?php echo $g[1]; ?>'>[ Change Pass ]</a><A href='?cpiarea=members&changename=<?php echo $g[1]; ?>'>[ Change Name ]</a> <?php if(isset($_GET['act'])&&$_GET['act']=="Validating") { echo "<A href='?cpiarea=members&validate=$g[1]&act=Validating&akey=$key'>[ Validate ]</a>"; } ?></div></td><td class=arcade1 align='left'><?php echo $g['email']; ?></td><td class=arcade1><?php echo $g['ipaddress']; ?></td><td class=arcade1><input type='checkbox' name='members_selected[]' value='<?php echo $g[1]; ?>'></td></tr>
