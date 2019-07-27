883c08b352ef0bf8ff0c6dce4481861cccd2b872
masodo <41118685+masodo@users.noreply.github.com>
2019-07-27 08:32:35 -0400

    Added mysqli_result() function

--- Functions.php
+++ Functions.php
@@ -11,7 +11,19 @@
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: Functions.php  Function: Some Common Functions   Modified: 7/26/2019   By: MaSoDo
+# Section: Functions.php  Function: Some Common Functions   Modified: 7/27/2019   By: MaSoDo
+
+function mysqli_result($res,$row=0,$col=0){ 
+    $numrows = mysqli_num_rows($res); 
+    if ($numrows && $row <= ($numrows-1) && $row >=0){
+        mysqli_data_seek($res,$row);
+        $resrow = (is_numeric($col)) ? mysqli_fetch_row($res) : mysqli_fetch_assoc($res);
+        if (isset($resrow[$col])){
+            return $resrow[$col];
+        }
+    }
+    return false;
+}
 
 function rangecheck($a){
 if (substr($a,-1)!=".") $a.=".";
