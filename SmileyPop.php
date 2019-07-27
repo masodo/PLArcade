b9d56ab326ce30b0969e27fa5460de5339b0ff9c
masodo <41118685+masodo@users.noreply.github.com>
2019-07-27 09:18:25 -0400

    PHP7+ Compatible - Converted

--- SmileyPop.php
+++ SmileyPop.php
@@ -11,21 +11,21 @@
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: SmileyPop.php  Function: Emoticon Picker Popup   Modified: 6/29/2019   By: MaSoDo
+# Section: SmileyPop.php  Function: Emoticon Picker Popup   Modified: 7/27/2019   By: MaSoDo
 
 if(isset($_GET['action']) && $_GET['action']=="emotes") {
 echo "<div class='tableborder' width='75%' style='margin-top: 10px; margin-right: auto; margin-left: auto;'><table width='100%' cellpadding='4' cellspacing='1'><tr><td width='60%' align='center' class='headertableblock'>Emote</td><td width='60%' align='center' class='headertableblock'>Symbol</td></tr><tr>";
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Incompatible Function Block #1
-$emotesdata = run_query("SELECT * FROM `phpqa_emotes`");
-while($smils=mysql_fetch_array($emotesdata)){ 
+//UpdatedFunction Block #1
+$emotesdata = run_iquery("SELECT * FROM phpqa_emotes");
+while($smils=mysqli_fetch_array($emotesdata)){ 
 echo "\n<tr onclick=\"window.opener.document.forms['boxform'].elements['senttext'].value+='".$smils['code']."'\"><td class='arcade1' align='left'><a title='".$smils['description']."'><img src=\"".$smiliesloc."/".$smils['filename']."\"></a><br /></td><td class='arcade1' align='center'>".$smils['code']."</td></tr>";
 }
 echo "</table></div>";
 die();
 }
-//END Incompatible Function Block #1
+//END UpdatedFunction Block #1
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 if(isset($_GET['action']) && $_GET['action']=="allshouts") {
@@ -35,11 +35,11 @@ echo "<div class='tableborder' width='75%' style='margin-top: 10px; margin-right
 
 //All Shouts Display
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Incompatible Function Block #2
-$selectshouts = run_query("SELECT `name`,`shout`,`id`,`ipa`,`tstamp` FROM `phpqa_shoutbox` ORDER BY id DESC");
-while($shts=mysql_fetch_array($selectshouts)){ 
-$emotesdata = run_query("SELECT * FROM `phpqa_emotes`");
-while($smilS=mysql_fetch_array($emotesdata)){
+//UpdatedFunction Block #2
+$selectshouts = run_iquery("SELECT name,shout,id,ipa,tstamp FROM phpqa_shoutbox ORDER BY id DESC");
+while($shts=mysqli_fetch_array($selectshouts)){ 
+$emotesdata = run_iquery("SELECT * FROM phpqa_emotes");
+while($smilS=mysqli_fetch_array($emotesdata)){
 $shts[1] = bbcodeHtml($shts[1]);
 if (isset($smilS['code'])) $shts[1] = str_replace(rtrim($smilS['code']), "<img src='".$smiliesloc."/".$smilS['filename']."' />", $shts[1]);
 }
@@ -50,13 +50,12 @@ $shts[1]= preg_replace("/$checkbadwords/i", "@!&^*%", $shts[1]);
 } 
 }
 $parse_stamp = date($datestamp, $shts[4] );
-$GtGp=run_query("SELECT `group` FROM `phpqa_accounts` WHERE `name`='".$shts[0]."'");
-$HvGp=mysql_fetch_array($GtGp);
+$GtGp=run_iquery("SELECT `group` FROM phpqa_accounts WHERE name='".$shts[0]."'");
+$HvGp=mysqli_fetch_array($GtGp);
 echo "<a title='".$parse_stamp."'><img src='".$imgloc."/clockin.png' alt='posted time' height='10' width='10' /></a>&nbsp; <u><b><a href='?action=profile&user=".$shts[0]."' class='".$HvGp[0]."Look'>".$shts[0]."</a></b></u>: ".$shts[1]."<br /><hr />";}
 echo "</td></tr></table></div>";
 die();
 }
-//END Incompatible Function Block #2
+//END UpdatedFunction Block #2
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 ?>
