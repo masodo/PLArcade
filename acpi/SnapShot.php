853621443ed1f7fa5ce216d93dc12d9b113e061b
masodo <41118685+masodo@users.noreply.github.com>
2019-07-27 09:44:47 -0400

    PHP7+ Compatible - Converted

--- acpi/SnapShot.php
+++ acpi/SnapShot.php
@@ -11,24 +11,24 @@
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: Leaderboards.php  Function: Display of Hall of Fame   Modified: 7/26/2019   By: MaSoDo
+# Section: Leaderboards.php  Function: Display of Hall of Fame   Modified: 7/27/2019   By: MaSoDo
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //		  SnapShot
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Incompatible Function Block #1
-$Wyears = run_query("SELECT DISTINCT `Wyear` FROM `phpqa_wall` ORDER BY `Wyear` ASC");
+//UpdatedFunction Block #1
+$Wyears = run_iquery("SELECT DISTINCT Wyear FROM phpqa_wall ORDER BY Wyear ASC");
 
 
-while($WYsee = mysql_fetch_array($Wyears)) {
+while($WYsee = mysqli_fetch_array($Wyears)) {
 $Wyear = date("Y");
 if ($WYsee['Wyear'] == $Wyear) { die("<script>alert('We Already Have This SnapShot!')</script>"); }
 }
 
-$Wscoreboard = run_query("SELECT `name`,`avatar`, COUNT(`phpqa_leaderboard`.`username`) AS `champions` FROM `phpqa_accounts` LEFT JOIN `phpqa_leaderboard` ON `phpqa_accounts`.`name` = `phpqa_leaderboard`.`username` GROUP BY `phpqa_leaderboard`.`username` ORDER BY `champions` DESC LIMIT 0,3");
-$Wscores=mysql_fetch_array($Wscoreboard);
+$Wscoreboard = run_iquery("SELECT name,avatar, COUNT(phpqa_leaderboard.username) AS champions FROM phpqa_accounts LEFT JOIN phpqa_leaderboard ON phpqa_accounts.name = phpqa_leaderboard.username GROUP BY phpqa_leaderboard.username ORDER BY champions DESC LIMIT 0,3");
+$Wscores=mysqli_fetch_array($Wscoreboard);
 
 
 if ($Wscores['avatar'] == ''){ $Wscores['avatar'] = $avatarloc.'/man.gif'; }
@@ -40,18 +40,18 @@ $Wgames = $Wscores['champions'];
 $Wname = $Wscores['name'];
 $Wavatar = $Wscores['avatar'];
 
-run_query("INSERT INTO `phpqa_wall` (`Wyear`,`Wplace`,`Wgames`,`Wname`,`Wavatar`) VALUES (".$Wyear.",".$Wplace.",".$Wgames.",'".$Wname."','".$Wavatar."')");
+run_iquery("INSERT INTO phpqa_wall (Wyear,Wplace,Wgames,Wname,Wavatar) VALUES (".$Wyear.",".$Wplace.",".$Wgames.",'".$Wname."','".$Wavatar."')");
 
-while($Wscores=mysql_fetch_array($Wscoreboard)){
+while($Wscores=mysqli_fetch_array($Wscoreboard)){
 $trop = $trop + 1;
 $Wyear = date("Y");
 $Wplace = $trop;
 $Wgames = $Wscores['champions'];
 $Wname = $Wscores['name'];
 $Wavatar = $Wscores['avatar'];
-run_query("INSERT INTO `phpqa_wall` (`Wyear`,`Wplace`,`Wgames`,`Wname`,`Wavatar`) VALUES (".$Wyear.",".$Wplace.",".$Wgames.",'".$Wname."','".$Wavatar."')");
+run_iquery("INSERT INTO phpqa_wall (Wyear,Wplace,Wgames,Wname,Wavatar) VALUES (".$Wyear.",".$Wplace.",".$Wgames.",'".$Wname."','".$Wavatar."')");
 }
-//END Incompatible Function Block #1
+//END UpdatedFunction Block #1
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
