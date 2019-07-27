d777242b7c1a101ad90b4b32fbfb59295dd5e7a6
masodo <41118685+masodo@users.noreply.github.com>
2019-07-27 08:12:18 -0400

    PHP7+ Compatible - Converted

--- MembersOption.php
+++ MembersOption.php
@@ -11,25 +11,20 @@
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: MembersOption.php  Function: Show the Members List   Modified: 7/26/2019   By: MaSoDo
+# Section: MembersOption.php  Function: Show the Members List   Modified: 7/27/2019   By: MaSoDo
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Incompatible Function Block #1
-$q=run_query("SELECT `name`,`group`,`skin`,`vtstamp` FROM phpqa_accounts ORDER BY name ASC");
+$q=run_iquery("SELECT name,group,skin,vtstamp FROM phpqa_accounts ORDER BY name ASC");
 echo "<table class='tableborder' style='width:1000px;'><tr><td class='headertableblock'>Username</td><td class='headertableblock'>Group</td><td class='headertableblock'>Skin</td><td class='headertableblock'>Last On <span style='font-weight:normal'>( &larr; <i>today</i> )</span></td></tr>";
 $LastOn='';
 $indicate='';
 $thisday=date('jS F Y');
-while ($f=mysql_fetch_array($q)){ 
+while ($f=mysqli_fetch_array($q)){ 
 if($f[3]!=0){
 $LastOn=date($datestamp,$f[3]);
 (date('jS F Y',$f[3])==$thisday)?$indicate='&larr;':$indicate='';
 } else { $LastOn=''; $indicate='';}
 echo "<tr><td class='arcade1'><a href='?action=profile&amp;user=".$f[0]."' class='".$f[1]."Look'>".$f[0]."</a></td><td class='arcade1'>".$f[1]."</td><td class='arcade1'>".$f[2]."</td><td class='arcade1'>".$LastOn." ".$indicate."</td></tr>";
 }
-$total=mysql_num_rows($q);
+$total=mysqli_num_rows($q);
 echo "<br /><table style='width:1000px;' cellpadding='4' cellspacing='1'class='tableborder'><tr><td class='arcade1'>Total Registered Members: $total</td></tr></table><br /></table><div align='center'>";
-//END Incompatible Function Block #1
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 ?>
