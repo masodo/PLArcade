c154e6476af3ebc9591607d0a468ad3fb68dfb1b
masodo <41118685+masodo@users.noreply.github.com>
2019-07-29 12:05:13 -0400

    PLA_[2.0](BETA)

--- acpi/editor.php
+++ acpi/editor.php
@@ -1,6 +1,6 @@
 <?php
 //-----------------------------------------------------------------------------------/
-//Practical-Lightning-Arcade [PLA] 2.0 (ALPHA) based on PHP-Quick-Arcade 3.0 © Jcink.com
+//Practical-Lightning-Arcade [PLA] 2.0 (BETA) based on PHP-Quick-Arcade 3.0 © Jcink.com
 //Tournaments & JS By: SeanJ. - Heavily Modified by PracticalLightning Web Design
 //Michael S. DeBurger [DeBurger Photo Image & Design]
 //-----------------------------------------------------------------------------------/
@@ -11,7 +11,7 @@
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: acpi Place: editor - Administrator Control Panel   Modified: 7/26/2019   By: MaSoDo
+# Section: acpi Place: editor - Administrator Control Panel   Modified: 5/26/2019   By: MaSoDo
 
 if(!isset($_GET['announce'])){
 { 
