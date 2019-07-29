f32b7af6e341fa8611635cac55a4714f3b3651a4
masodo <41118685+masodo@users.noreply.github.com>
2019-07-29 11:01:27 -0400

    PLA_[2.0](BETA)

--- ForumGate.php
+++ ForumGate.php
@@ -1,6 +1,6 @@
 <?php
 //-----------------------------------------------------------------------------------/
-//Practical-Lightning-Arcade [PLA] 2.0 (ALPHA) based on PHP-Quick-Arcade 3.0 © Jcink.com
+//Practical-Lightning-Arcade [PLA] 2.0 (BETA) based on PHP-Quick-Arcade 3.0 © Jcink.com
 //Tournaments & JS By: SeanJ. - Heavily Modified by PracticalLightning Web Design
 //Michael S. DeBurger [DeBurger Photo Image & Design]
 //-----------------------------------------------------------------------------------/
@@ -11,24 +11,20 @@
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: ForumGate.php Function: GoTo Forum   Modified: 7/26/2019   By: MaSoDo
+# Section: ForumGate.php Function: GoTo Forum   Modified: 7/29/2019   By: MaSoDo
 
 $time=time();
 if (isset($_COOKIE['phpqa_user_c'])) {
 $phpqa_user_cookie = $_COOKIE['phpqa_user_c'];
 $w="Visiting the Forums";}
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Updated Function Block #1
 $areyouthere=@mysqli_fetch_array(run_iquery("SELECT name FROM phpqa_sessions WHERE name='".$phpqa_user_cookie."'"));
 if(!$areyouthere) {
 global $w; 
 run_iquery("INSERT INTO phpqa_sessions (name,time,location) VALUES ('$phpqa_user_cookie','$time','$w')"); 
 } else {
 global $w; 
-$areyouthere=run_iquery("UPDATE `phpqa_sessions` SET `time` = '$time', `location` = '$w' WHERE name='$phpqa_user_cookie'");
+$areyouthere=run_iquery("UPDATE phpqa_sessions SET time = '$time', location = '$w' WHERE name='$phpqa_user_cookie'");
 }
-//END Updated Function Block #1
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 header("Location: /FORUM/");
 ?>
