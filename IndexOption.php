92ed646a00be80541aace654e41650321925e6a5
masodo <41118685+masodo@users.noreply.github.com>
2019-07-29 11:05:40 -0400

    PLA_[2.0](BETA)

--- IndexOption.php
+++ IndexOption.php
@@ -1,17 +1,17 @@
 <?php
 //-----------------------------------------------------------------------------------/
-//Practical-Lightning-Arcade [PLA] 2.0 (ALPHA) based on PHP-Quick-Arcade 3.0 Â© Jcink.com
+//Practical-Lightning-Arcade [PLA] 2.0 (BETA) based on PHP-Quick-Arcade 3.0 © Jcink.com
 //Tournaments & JS By: SeanJ. - Heavily Modified by PracticalLightning Web Design
 //Michael S. DeBurger [DeBurger Photo Image & Design]
 //-----------------------------------------------------------------------------------/
-//  phpQuickArcade v3.0.x Â© Jcink 2005-2010 quickarcade.jcink.com                        
+//  phpQuickArcade v3.0.x © Jcink 2005-2010 quickarcade.jcink.com                        
 //
 //  Version: 3.0.23 Final. Released: Sunday, May 02, 2010
 //-----------------------------------------------------------------------------------/
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: IndexOption.php  Function: Display Games Index   Modified: 7/26/2019   By: MaSoDo
+# Section: IndexOption.php  Function: Display Games Index   Modified: 7/29/2019   By: MaSoDo
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	//		  Favorites
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -19,8 +19,6 @@ if (isset($_GET['action']) && $_GET['action'] == "fav") {
 vsess();
 $_GET['game'] = htmlspecialchars($_GET['game'], ENT_QUOTES);
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Updated Function Block #1
 $game_exist=mysqli_fetch_array(run_iquery("SELECT id FROM phpqa_games WHERE gameid='{$_GET['game']}'"));
 if($game_exist[0]) {
 //Adding?
@@ -39,9 +37,6 @@ run_iquery("UPDATE phpqa_accounts SET settings = '$acct_setting[0]|$acct_setting
 message("Game not found.");
 }
 }
-//END Updated Function Block #1
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	//		  Game index display
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -54,8 +49,6 @@ if (!empty($catquer)) {
 echo "<div style='text-align:center; margin-bottom: 5px; margin-top: 5px;'><a title='Open/Close The Games Index'><img id='btn4' src='" . $imgloc . "/" . $collimg4 . "' type='button' alt='&#8595; Games: Collapse/Expand &#8595;' onclick='return CollapseExpand4()' style='font-size:16px; font-weight:bold; color:silver;' /></a></div><div id='MyDiv4' class='" . $collapset4 . "'>";
 require "./PageMakerT.php";
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Updated Function Block #2
 	while($g=mysqli_fetch_array($catquer)){ 
 	// Select from the scores table....
 	$CheckScoring = $g['scoring'];
@@ -64,9 +57,6 @@ require "./PageMakerT.php";
 if ($g['gamecat'] != '2' || (isset($exist[6])&&$exist[6] == "Admin")) {
 	$CHMP = run_iquery("SELECT avatar,`group` FROM phpqa_accounts WHERE name = '" . $g['Champion_name'] . "'");
         $CHMPimg=mysqli_fetch_array($CHMP);
-//END Updated Function Block #2
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 
 if (!$CHMPimg['avatar'])$CHMPimg['avatar'] = $avatarloc.'/man.gif';
 if ($g['platform'] == 'H5') { 
