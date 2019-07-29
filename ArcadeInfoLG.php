a6d8c61a0796dd62074a03649ea4d7be8e7a8a5e
masodo <41118685+masodo@users.noreply.github.com>
2019-07-29 10:47:08 -0400

    PLA_[2.0](BETA)

--- ArcadeInfoLG.php
+++ ArcadeInfoLG.php
@@ -1,6 +1,6 @@
 <?php
 //-----------------------------------------------------------------------------------/
-//Practical-Lightning-Arcade [PLA] 2.0 (ALPHA) based on PHP-Quick-Arcade 3.0 © Jcink.com
+//Practical-Lightning-Arcade [PLA] 2.0 (BETA) based on PHP-Quick-Arcade 3.0 © Jcink.com
 //Tournaments & JS By: SeanJ. - Heavily Modified by PracticalLightning Web Design
 //Michael S. DeBurger [DeBurger Photo Image & Design]
 //-----------------------------------------------------------------------------------/
@@ -11,7 +11,7 @@
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: ArcadeInfoLG.php By: Legionaire Function: Latest Site Info Block   Modified: 7/26/2019   By: MaSoDo
+# Section: ArcadeInfoLG.php By: Legionaire Function: Latest Site Info Block   Modified: 7/29/2019   By: MaSoDo
 ?>
 <br />
 <?php
@@ -23,8 +23,6 @@ $forumnote = '';
 if (isset($_COOKIE['phpqa_user_c'])) {
 $MyMess = $_COOKIE['phpqa_user_c'];
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Updated Function Block #1
 $checkmessages=run_iquery("SELECT id FROM PLA_users where username = '".$MyMess."'"); 
 $checkmess= mysqli_fetch_array($checkmessages);
 $checkmess_for = $checkmess['id'];
@@ -38,9 +36,6 @@ $anyOsent=mysqli_num_rows(run_iquery("SELECT status FROM PLA_pun_pm_messages WHE
 if($anyOsent > 0){ $AlertMe='yes'; $alertstyle='border:solid orange;'; $alertlink='color:orange;'; $forumnote=' (<b>PM</b> alt)'; }
 }
 }
-//END Updated Function Block #1
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 ?>
 <table align="center" width="1200" cellpadding="4" cellspacing="1">
 <tr>
@@ -54,13 +49,8 @@ if (!isset($_COOKIE['phpqa_user_c'])) {
 <?php
 } else {
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Updated Function Block #2
 $checkChamps = mysqli_num_rows(run_iquery("SELECT username FROM phpqa_leaderboard WHERE username = '$phpqa_user_cookie'"));
 echo "<div style='width: 100%; text-align: center; margin-left: auto; margin-right: auto;'><div  class='navigation'>Logged in: <A href='index.php?action=profile&amp;user=$phpqa_user_cookie'>$phpqa_user_cookie</a> Total Wins: $checkChamps </div><div class='navigation'><a href='index.php?action=settings'>Settings</a>";
-//END Updated Function Block #2
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 if ($exist[6]=="Admin") {
 echo " (<a href='index.php?cpiarea=idx'><b>Admin CP</b></a>) &middot; (<a href='index.php?modcparea=idx'><b>Mod CP</b></a>)";
 }
@@ -75,12 +65,7 @@ echo"</div><div  class='navigation'>[ <a href='index.php?action=logout'>Log Out<
 </tr></table>
 <div style="display:none; width:250px; margin-left:auto; margin-right:auto;" class='tableborder' id="search" name="tog_collect"><br />Search The Arcade<br /><br /><form method="get">Term: <input type="text" name="search" value="" /><br />Search By: <select name="by"><?php foreach(Array('game'=>'Game Name','gameid'=>'Game ID','about'=>'Game desc','Champion_name'=>'Champion Name') as $k=>$v) echo "<option value='$k'>$v</option>";?></select><br />In Category: <select size="1" name="searchcat"><option value='All' selected="selected">All</option><?php 
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Updated Function Block #3
 $catquery=run_iquery("SELECT * FROM phpqa_cats ORDER BY displayorder");while ($catlist= mysqli_fetch_array($catquery)) echo  "<option value='$catlist[0]'>$catlist[1]</option>"; 
-//END Updated Function Block #3
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 ?></select><br /><input type="submit" value="search" name="action" /></form></div>
 <div align="center">
 <?php // Begin Collapse #1 
@@ -126,14 +111,9 @@ if (!isset($acct_setting[3]) || $acct_setting[3] !="No") {
 <td class="arcade1" valign="top" align="left">
 <?php
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Updated Function Block #4
 $newgames = run_iquery("SELECT gameid,game,id,gamecat FROM phpqa_games ORDER by id DESC LIMIT 0,".$ngnum."");
 	while($g=mysqli_fetch_array($newgames)){ 
 	if (($g[3] != '23')&&($g[3] != '2')){
-//END Updated Function Block #4
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 echo "<img height='20' width='20' src='arcade/pics/$g[0].gif' alt='$g[1]' /><a href=\"index.php?play=$g[0]#playzone\">$g[1]</a><br />";
 }}
 ?>
@@ -143,8 +123,6 @@ echo "<img height='20' width='20' src='arcade/pics/$g[0].gif' alt='$g[1]' /><a h
 <table width="100%"><td class="arcade1" valign="top" align="left">
 <?php
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Updated Function Block #4
 $scoreboard = run_iquery("SELECT phpqa_accounts.name,phpqa_accounts.avatar,phpqa_accounts.group, COUNT(phpqa_leaderboard.username) AS champions FROM phpqa_accounts
 LEFT JOIN phpqa_leaderboard ON phpqa_accounts.name = phpqa_leaderboard.username
 GROUP BY phpqa_leaderboard.username
@@ -164,9 +142,6 @@ if ($trop == 3) { $trophy = "<img src='$crowndir/crown3.gif' style='margin-left:
 if ($scores['avatar'] == ''){ $scores['avatar'] = $avatarloc.'/man.gif'; }
 echo "<td class='arcade1'><div align='center'><i><span style='font-size: 14px; line-height:175%'><a href=\"index.php?action=profile&amp;user=".$scores['name']."\"><img src='".$scores['avatar']."' height='60px' /><br />" . $trophy . "<span class='".$scores['group']."Look'>".$scores['name']."</span>" . $trophy . "</a></i><br /><div padding:3px; background-color:navy;' align='center'>with <b>".$scores['champions']."</b> wins!</div></div></td>";
 }
-//END Updated Function Block #4
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 ?>
 </td>
 </tr>
@@ -179,8 +154,6 @@ echo "<td class='arcade1'><div align='center'><i><span style='font-size: 14px; l
 <?php
 echo "";
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Updated Function Block #5
 	$selectfrom = run_iquery("SELECT * FROM phpqa_scores ORDER BY phpdate DESC LIMIT 0,$lsnum");
 	while($s=mysqli_fetch_array($selectfrom)){ 
   $VstatG = "";
@@ -200,23 +173,15 @@ $thisGroup = mysqli_fetch_array($findGroup);
 $parse_stamp = date($datestamp, $s[5]);
 echo "$VstatG<a href='index.php?action=profile&amp;user=".$s[1]."' class='".$thisGroup[0]."Look'>".$s[1]."</a>" . $bigtag . " scored <i>" . str_replace('-', '', $s[2]) . "</i> in <a href='index.php?id=".$s[6]."#playzone'><i>$s[7]</i></a> on ".$parse_stamp."<hr>";
 }
-//END Updated Function Block #5
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 ?>
 </td><td class="arcade1" valign="top" align="left">
 <?php
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Updated Function Block #6
 $hotgames = run_iquery("SELECT gameid,game,id,gamecat,times_played  FROM phpqa_games ORDER by times_played DESC LIMIT $lsnum");
 	while($hg=mysqli_fetch_array($hotgames)){ 
 	if ($hg[3] != '2') {
 echo "<img height='20' width='20' src='arcade/pics/$hg[0].gif' alt='$hg[1]' style='margin-left:5px;' /><a href=\"index.php?play=$hg[0]#playzone\">$hg[1]</a> (".$hg['times_played'].")<br />";
 }}
-//END Updated Function Block #6
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 
 } // End acct based check for big table
 } // End check for big table
@@ -231,4 +196,3 @@ if (isset($settings["show_stats_table"])&&$settings["show_stats_table"]=='1') {
 echo "</div>";
 }
 ?>
-
