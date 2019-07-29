9deea6df547083e14f2ceca04d50fe2a1cccf97b
masodo <41118685+masodo@users.noreply.github.com>
2019-07-29 11:13:57 -0400

    PLA_[2.0](BETA)

--- OnlineListOption.php
+++ OnlineListOption.php
@@ -1,6 +1,6 @@
 <?php
 //-----------------------------------------------------------------------------------/
-//Practical-Lightning-Arcade [PLA] 2.0 (ALPHA) based on PHP-Quick-Arcade 3.0 © Jcink.com
+//Practical-Lightning-Arcade [PLA] 2.0 (BETA) based on PHP-Quick-Arcade 3.0 © Jcink.com
 //Tournaments & JS By: SeanJ. - Heavily Modified by PracticalLightning Web Design
 //Michael S. DeBurger [DeBurger Photo Image & Design]
 //-----------------------------------------------------------------------------------/
@@ -11,7 +11,7 @@
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: OnlineListOption.php  Function: Maintain Online User List   Modified: 7/27/2019   By: MaSoDo
+# Section: OnlineListOption.php  Function: Maintain Online User List   Modified: 7/29/2019   By: MaSoDo
 
 			// = = = = = = = = = = = = = = = = = = 
 			// 		Online List
@@ -30,18 +30,14 @@ if(isset($_GET['action']) && $_GET['action'] == "leaderboards") $w="Viewing The 
 if(isset($_GET['action']) && $_GET['action'] == "members") $w="Viewing The Members List";
 }
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//UpdatedFunction Block #1
 $areyouthere=@mysqli_fetch_array(run_iquery("SELECT name FROM phpqa_sessions WHERE name='".$phpqa_user_cookie."'"));
 if(!$areyouthere) {
 global $w; 
 run_iquery("INSERT INTO phpqa_sessions (name,time,location) VALUES ('$phpqa_user_cookie','$time','$w')"); 
 } else {
 global $w; 
-$areyouthere=run_iquery("UPDATE `phpqa_sessions` SET `time` = '$time', `location` = '$w' WHERE name='$phpqa_user_cookie'");
+$areyouthere=run_iquery("UPDATE phpqa_sessions SET time = '$time', location = '$w' WHERE name='$phpqa_user_cookie'");
 }
-//END UpdatedFunction Block #1
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // End Collapse #4
 echo "</div>";
@@ -50,12 +46,9 @@ echo "</div>";
 }
 if(!isset($_GET['id']) && !isset($_GET['cparea']) && !isset($_GET['modcparea']) && !isset($_GET['play'])) $w="Viewing Arcade Index"; 
 echo "<div class='tableborder'><table width='100%' cellpadding='4' cellspacing='1'><tr><td class='arcade1' align='left'><fieldset class=\"search\"><legend>Users Online in the Past ".$settings['online_list_dur']." Minutes: (<a href='./index.php?action=Online&method=time' target='_top'>Last Click</a>, <a href='./index.php?action=Online&method=name' target='_top'>Member Name</a>) <span class='adminLook'>Administrator</span>  <span class='moderatorLook'>Moderator</span>  <span class='memberLook'>Member</span>  <span class='affiliateLook'>Affiliate</span> </legend><br />";
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//UpdatedFunction Block #2
 $online=run_iquery("SELECT * FROM phpqa_sessions ORDER by time DESC");
 	while($g=mysqli_fetch_array($online)){ 
-	$onnow = run_iquery("SELECT `group` FROM `phpqa_accounts` WHERE `name` = '" . $g['name'] . "'");
+	$onnow = run_iquery("SELECT `group` FROM phpqa_accounts WHERE name = '" . $g['name'] . "'");
         $onnowGrp=mysqli_fetch_array($onnow);
 $time = time();
 $lasttime = ($g['time']);
@@ -63,9 +56,6 @@ $HowManyMinutes=round(abs($lasttime-$time) / 60,2);
 echo "</div>";
 if($HowManyMinutes > $settings['online_list_dur']) { 
 run_iquery("DELETE FROM phpqa_sessions WHERE name='".$g['name']."'");
-//END UpdatedFunction Block #2
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 } else {
 $where=($g['location']);
 if ($where=='')$where="Viewing Arcade Index";
