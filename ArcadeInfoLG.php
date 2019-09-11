2af4ab2062ce683c75f8643203e139cd516def21
masodo <41118685+masodo@users.noreply.github.com>
2019-09-11 12:27:57 -0400

    Beauty Format Big Scores

--- ArcadeInfoLG.php
+++ ArcadeInfoLG.php
@@ -1,17 +1,17 @@
 <?php
 //-----------------------------------------------------------------------------------/
-//Practical-Lightning-Arcade [PLA] 2.0 (BETA) based on PHP-Quick-Arcade 3.0 © Jcink.com
+//Practical-Lightning-Arcade [PLA] 2.0 (BETA) based on PHP-Quick-Arcade 3.0 Â© Jcink.com
 //Tournaments & JS By: SeanJ. - Heavily Modified by PracticalLightning Web Design
 //Michael S. DeBurger [DeBurger Photo Image & Design]
 //-----------------------------------------------------------------------------------/
-//  phpQuickArcade v3.0.x © Jcink 2005-2010 quickarcade.jcink.com                        
+//  phpQuickArcade v3.0.x Â© Jcink 2005-2010 quickarcade.jcink.com                        
 //
 //  Version: 3.0.23 Final. Released: Sunday, May 02, 2010
 //-----------------------------------------------------------------------------------/
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: ArcadeInfoLG.php By: Legionaire Function: Latest Site Info Block   Modified: 7/30/2019   By: MaSoDo
+# Section: ArcadeInfoLG.php By: Legionaire Function: Latest Site Info Block   Modified: 9/11/2019   By: MaSoDo
 ?>
 <br />
 <?php
@@ -171,7 +171,7 @@ $findGroup = run_iquery("SELECT `group` FROM phpqa_accounts WHERE name = '".$thi
 $thisGroup = mysqli_fetch_array($findGroup);
 //$parse_stamp = gmdate($datestamp, $s[5]+3600*$settings['timezone']);
 $parse_stamp = date($datestamp, $s[5]);
-echo "$VstatG<a href='index.php?action=profile&amp;user=".$s[1]."' class='".$thisGroup[0]."Look'>".$s[1]."</a>" . $bigtag . " scored <i>" . str_replace('-', '', $s[2]) . "</i> in <a href='index.php?id=".$s[6]."#playzone'><i>$s[7]</i></a> on ".$parse_stamp."<hr>";
+echo "$VstatG<a href='index.php?action=profile&amp;user=".$s[1]."' class='".$thisGroup[0]."Look'>".$s[1]."</a>" . $bigtag . " scored <i>" . Beautify(str_replace('-', '', $s[2])) . "</i> in <a href='index.php?id=".$s[6]."#playzone'><i>$s[7]</i></a> on ".$parse_stamp."<hr>";
 }
 ?>
 </td><td class="arcade1" valign="top" align="left">
