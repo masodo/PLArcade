6957bbbff8ca0a9f76d04318200131699e365f88
masodo <41118685+masodo@users.noreply.github.com>
2019-11-02 09:34:22 -0400

    RelaxArcade Imports

--- acpi.php
+++ acpi.php
@@ -1,17 +1,17 @@
 <?php
 //-----------------------------------------------------------------------------------/
-//Practical-Lightning-Arcade [PLA] 2.0 (BETA) based on PHP-Quick-Arcade 3.0 © Jcink.com
+//Practical-Lightning-Arcade [PLA] 2.0 (BETA) based on PHP-Quick-Arcade 3.0 Â© Jcink.com
 //Tournaments & JS By: SeanJ. - Heavily Modified by PracticalLightning Web Design
 //Michael S. DeBurger [DeBurger Photo Image & Design]
 //-----------------------------------------------------------------------------------/
-//  phpQuickArcade v3.0.x © Jcink 2005-2010 quickarcade.jcink.com                        
+//  phpQuickArcade v3.0.x Â© Jcink 2005-2010 quickarcade.jcink.com                        
 //
 //  Version: 3.0.23 Final. Released: Sunday, May 02, 2010
 //-----------------------------------------------------------------------------------/
 // Thanks to (Sean) http://seanj.jcink.com 
 // for: Tournies, JS, and more
 // ---------------------------------------------------------------------------------/
-# Section: acp.php  Function: (Experimental) Administrator Control Panel   Modified: 7/29/2019   By: MaSoDo
+# Section: acpi.php  Function: Administrator Control Panel   Modified: 10/31/2019   By: MaSoDo
 $place = htmlspecialchars($_GET['cpiarea']); 
 if (isset($_REQUEST['acpcheck'])) die();
 if (isset($_COOKIE['acpcheck'])) die();
@@ -24,6 +24,8 @@ require "acpi/addgames.php";
 require "acpi/tar_import.php";
 } elseif($place == "tar_importH5") {
 require "acpi/tar_importH5.php";
+} elseif($place == "tar_importRA") {
+require "acpi/tar_importRA.php";
 } elseif($place == "skin") {
 require "acpi/skin.php";
 } 
